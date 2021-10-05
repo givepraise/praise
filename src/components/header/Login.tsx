9ee6ded6ec0c80ba8840b9c9040472d9c3c7b46d
Kristofer Lund <kristofer@fmckl.se>
2021-10-05 23:04:00 +0200

    login flow

--- src/components/header/Login.tsx
+++ src/components/header/Login.tsx
@@ -1,45 +1,57 @@
 import { generateLoginMessage, saveSession } from "@/spring/auth";
-import { NonceQuery } from "@/store/index";
+import { AuthQuery, NonceQuery } from "@/store/index";
 import { useWeb3React } from "@web3-react/core";
+import React from "react";
 import { useRecoilValue } from "recoil";
+
 export default function LoginButton() {
-  const { account: ethAccount, library: ethLibrary } = useWeb3React();
+  const LoginButtonInner = () => {
+    const { account: ethAccount, library: ethLibrary } = useWeb3React();
+    const [message, setMessage] = React.useState<string | any>(undefined);
+    const [signature, setSignature] = React.useState<string | any>(undefined);
 
-  const dummyFetchNonce = () => {
-    return "NONCE123";
-  };
-
-  const dummyVerifySignature = (signature: string) => {
-    return "JWT_SESSION_ID";
-  };
-
-  const nonce = useRecoilValue(NonceQuery({ ethAccount }));
-
-  const loginToPraise = async () => {
-    // 1. Fetch current nonce from server
-    let nonce = dummyFetchNonce();
-
-    // 2. Generate login message to sign
-    const loginMessage = generateLoginMessage(ethAccount!, nonce);
-
-    // 3. Sign the message using Metamask
-    const signature: any = await ethLibrary
-      .getSigner()
-      .signMessage(loginMessage);
+    // 1. Fetch nonce from server
+    const nonce = useRecoilValue(NonceQuery({ ethAccount }));
 
     // 4. Verify signature with server
-    const jwtSession = dummyVerifySignature(signature);
+    const session = useRecoilValue(
+      AuthQuery({ ethAccount, message, signature })
+    );
+
+    // 2. Generate login message to sign
+    React.useEffect(() => {
+      if (!ethAccount || !nonce) return;
+      setMessage(generateLoginMessage(ethAccount, nonce));
+    }, [ethAccount, nonce]);
 
     // 5. Save session id for future api calls
-    saveSession(ethAccount!, jwtSession);
+    React.useEffect(() => {
+      if (!ethAccount || !session) return;
+      console.log(JSON.stringify(session));
+      saveSession(ethAccount, session);
+    }, [ethAccount, session]);
+
+    const signLoginMessage = async () => {
+      // 3. Sign the message using Metamask
+      const _signature: any = await ethLibrary.getSigner().signMessage(message);
+      if (_signature) setSignature(_signature);
+    };
+
+    if (!nonce) return null;
+
+    return (
+      <button
+        className="inline-block px-3 py-2 mr-4 text-base font-semibold text-white uppercase bg-green-700 rounded-lg"
+        onClick={signLoginMessage}
+      >
+        Login to Praise
+      </button>
+    );
   };
 
   return (
-    <button
-      className="inline-block px-3 py-2 mr-4 text-base font-semibold text-white uppercase bg-green-700 rounded-lg"
-      onClick={loginToPraise}
-    >
-      Login to Praise
-    </button>
+    <React.Suspense fallback={null}>
+      <LoginButtonInner />
+    </React.Suspense>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,20 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { atom, selectorFamily } from "recoil";
 
 const apiGet = async (endPoint: string) => {
   if (!process.env.REACT_APP_BACKEND_URL) {
     console.log("BACKEND URL NOT SET");
   }
-  return axios.get(endPoint, {
-    baseUrl: `${!process.env.REACT_APP_BACKEND_URL}`,
-  } as AxiosRequestConfig);
+
+  return axios.get(`${process.env.REACT_APP_BACKEND_URL}${endPoint}`);
+};
+
+const apiPost = async (endPoint: string, data: any) => {
+  if (!process.env.REACT_APP_BACKEND_URL) {
+    console.log("BACKEND URL NOT SET");
+  }
+
+  return axios.post(`${process.env.REACT_APP_BACKEND_URL}${endPoint}`, data);
 };
 
 export const LoginNonceState = atom({
@@ -20,11 +27,33 @@ export const NonceQuery = selectorFamily({
   get:
     (params: any) =>
     async ({ get }) => {
-      let response = await apiGet(
+      const response = (await apiGet(
         `/api/auth/nonce?publicKey=${params.ethAccount}`
-      );
-      console.log(JSON.stringify(response));
-      //return response!.nonce;
-      return "1234";
+      )) as any;
+
+      // ADD ERROR HANDLING
+      // ADD TYPE CHECKING
+      return response!.data!.nonce;
+    },
+});
+
+export const AuthQuery = selectorFamily({
+  key: "AuthQuery",
+  get:
+    (params: any) =>
+    async ({ get }) => {
+      if (!params.ethAccount || !params.message || !params.signature)
+        return undefined;
+
+      const data = {
+        publicKey: params.ethAccount,
+        message: params.message,
+        signature: params.signature,
+      };
+      const response = (await apiPost(`/api/auth`, data)) as any;
+
+      // ADD ERROR HANDLING
+      // ADD TYPE CHECKING
+      return response!.data!;
     },
 });
