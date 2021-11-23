c86097cfd71eff8391799c6f0946e85f05c4421e
Kristofer Lund <kristofer@fmckl.se>
2021-11-23 09:26:23 +0100

    Safety check on shortenEthAddress

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,6 +31,7 @@ export const classNames = (...classes: any[]) => {
 };
 
 export const shortenEthAddress = (address: string) => {
+  if (!address || !address.length) return null;
   return `${address.substring(0, 6)}...${address.substring(
     address.length - 4
   )}`;
