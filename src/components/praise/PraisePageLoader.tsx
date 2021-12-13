7f5cf5c6d5d42da73c4cc91d3bf40aa74890e798
Kristofer Lund <kristofer@fmckl.se>
2021-12-13 16:34:00 +0100

    Added spinner and page check

--- src/components/praise/PraisePageLoader.tsx
+++ src/components/praise/PraisePageLoader.tsx
@@ -1,34 +1,46 @@
 import { AllPraisesQueryPagination, useAllPraisesQuery } from "@/model/praise";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback } from "react";
+import { useBottomScrollListener } from "react-bottom-scroll-listener";
 import { useRecoilState } from "recoil";
-import { useBottomScrollListener } from 'react-bottom-scroll-listener';
-import { useCallback, useEffect } from "react";
 
 const PraisePageLoader = () => {
-    const [praisePagination, setPraisePagination] = useRecoilState(
-        AllPraisesQueryPagination
-    );
+  const [praisePagination, setPraisePagination] = useRecoilState(
+    AllPraisesQueryPagination
+  );
 
-    useAllPraisesQuery({ page: praisePagination.currentPageNumber, size: 10 });
+  useAllPraisesQuery({ page: praisePagination.currentPageNumber, size: 10 });
 
-    const handleContainerOnBottom = useCallback(() => {
-        console.log('HERE');
-
-        setPraisePagination({
-            ...praisePagination,
-            currentPageNumber: praisePagination.currentPageNumber + 1,
-        })
-      }, [praisePagination, setPraisePagination]);
-    
-    /* This will trigger handleOnDocumentBottom when the body of the page hits the bottom */
-    useBottomScrollListener(handleContainerOnBottom, {
-        triggerOnNoScroll: true,
-        debounce: 2000
-    });         
-    
+  const handleContainerOnBottom = useCallback(() => {
     if (praisePagination.currentPageNumber >= praisePagination.totalPages - 1)
-        return null;             
+      return;
 
-    return (<div></div>);
-}
+    setPraisePagination({
+      ...praisePagination,
+      currentPageNumber: praisePagination.currentPageNumber + 1,
+    });
+  }, [praisePagination, setPraisePagination]);
+
+  /* This will trigger handleOnDocumentBottom when the body of the page hits the bottom */
+  useBottomScrollListener(handleContainerOnBottom, {
+    triggerOnNoScroll: true,
+    debounce: 2000,
+  });
+
+  if (praisePagination.currentPageNumber >= praisePagination.totalPages - 1)
+    return null;
+
+  return (
+    <div className="w-full mt-2 text-center">
+      <FontAwesomeIcon
+        icon={faSpinner}
+        size="1x"
+        spin
+        className="inline-block mr-4"
+      />
+    </div>
+  );
+};
 
 export default PraisePageLoader;
