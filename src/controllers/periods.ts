815338ef0aeebc477409df1d3f22480b3202f137
Kristofer Lund <kristofer@fmckl.se>
2021-12-28 00:42:44 +0100

    A few period endpoints

--- src/controllers/periods.ts
+++ src/controllers/periods.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express';
 import PeriodModel from '@entities/Period';
-import { QueryInput } from '@shared/inputs';
 import { getQuerySort } from '@shared/functions';
+import { QueryInput } from '@shared/inputs';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
-// get periods
-const getPeriods = async (
+export const all = async (
   req: Request<any, QueryInput, any>,
   res: Response
 ): Promise<Response> => {
@@ -12,7 +12,52 @@ const getPeriods = async (
     ...req.query,
     sort: getQuerySort(req.query),
   });
-  return res.status(200).json(response);
+  return res.status(StatusCodes.OK).json(response);
 };
 
-export default { getPeriods };
+export const single = async (
+  req: Request<any, QueryInput, any>,
+  res: Response
+): Promise<Response> => {
+  let period = await PeriodModel.findById(req.params.periodId);
+  if (!period) return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+  return res.status(StatusCodes.OK).json(period);
+};
+
+export const update = async (
+  req: Request<any, QueryInput, any>,
+  res: Response
+): Promise<Response> => {
+  const name = req.query.name as string;
+  const endDate = req.query.endDate as string;
+
+  if (!name && !endDate) return res.status(StatusCodes.BAD_REQUEST);
+
+  let period = await PeriodModel.findById(req.params.periodId);
+  if (!period) return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+
+  if (name) period.name = name;
+  if (endDate) {
+    const d = new Date(endDate);
+    if (d.toString() === 'Invalid Date')
+      return res.status(StatusCodes.BAD_REQUEST).send('Invalid date format.');
+    period.endDate = d;
+  }
+
+  period.save();
+
+  return res.status(StatusCodes.OK).json(period);
+};
+
+export const close = async (
+  req: Request<any, QueryInput, any>,
+  res: Response
+): Promise<Response> => {
+  let period = await PeriodModel.findById(req.params.periodId);
+  if (!period) return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+
+  period.status = 'CLOSED';
+  period.save();
+
+  return res.status(StatusCodes.OK).json(period);
+};
