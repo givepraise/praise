fcc55e7d1b4cf3ec71bf75e4e56516754185a3b7
Kristofer Lund <kristofer@fmckl.se>
2021-12-29 23:32:17 +0100

    Endpoint rename praise/all

--- src/routes/praise.ts
+++ src/routes/praise.ts
@@ -1,11 +1,11 @@
-import controller from '../controllers/praise';
 import { UserRole } from '@entities/User';
 import { authMiddleware } from '@middleware/auth';
 import { Router } from 'express';
+import controller from '../controllers/praise';
 
 // Praise-routes
 const praiseRouter = Router();
-praiseRouter.get('/', authMiddleware(UserRole.user), controller.all);
+praiseRouter.get('/all', authMiddleware(UserRole.user), controller.all);
 praiseRouter.get('/:id', authMiddleware(UserRole.user), controller.single);
 
 export = praiseRouter;
