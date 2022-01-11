38d694f2d5c94fd1108e25dd2067df05db07a0a1
Nebojsa Stojanovic <nebojsa.stojanovic0@gmail.com>
2022-01-11 09:51:06 +0100

    /set endpoint only for admins

--- src/routes/admin/settings.ts
+++ src/routes/admin/settings.ts
@@ -1,10 +1,16 @@
 import * as settingsController from '@controllers/settings';
+import { UserRole } from '@entities/User';
+import { authMiddleware } from '@middleware/auth';
 import { Router } from 'express';
 
 // Settings-routes
 const settingsRouter = Router();
 settingsRouter.get('/all', settingsController.all);
 settingsRouter.get('/:key', settingsController.single);
-settingsRouter.patch('/:key/set', settingsController.set);
+settingsRouter.patch(
+  '/:key/set',
+  authMiddleware(UserRole.QUANTIFIER),
+  settingsController.set
+);
 
 export = settingsRouter;
