358139f111c49bb6f46ac20c67ea16edae23f3c4
Kristofer Lund <kristofer@fmckl.se>
2022-01-12 19:58:54 +0100

    Username

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,14 +1,17 @@
 import { User, UserAccountPlatform } from "@/model/users";
+import { shortenEthAddress } from ".";
 
 export const getUsername = (user: User): string | undefined => {
   if (!user || !Array.isArray(user.accounts) || user.accounts.length === 0)
     return undefined;
 
-  let username;
+  let username = "";
   for (const account of user.accounts) {
     username = account.username;
     // Prefer DISCORD over others
     if (account.platform === UserAccountPlatform.DISCORD) break;
   }
+  const eth = shortenEthAddress(user.ethereumAddress);
+  if (username === "" && eth) return eth;
   return username;
 };
