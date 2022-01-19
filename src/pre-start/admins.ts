d80948c8522101e4780a28cffbff9b13a1a17975
Nebojsa Stojanovic <nebojsa.stojanovic0@gmail.com>
2022-01-19 16:25:06 +0100

    pre start user role fixed

--- src/pre-start/admins.ts
+++ src/pre-start/admins.ts
@@ -2,20 +2,25 @@ import UserModel, { UserRole } from '@entities/User';
 
 const seedAdmins = () => {
   const admins = process.env.ADMINS as string;
-  const ethAddresses = admins.split(',').join('').split('');
+  const ethAddresses = admins
+    .split(',')
+    .filter(Boolean)
+    .map((item) => {
+      return item.trim();
+    });
 
   ethAddresses.forEach(async (e) => {
     const user = await UserModel.findOne({ ethereumAddress: e });
 
     if (user) {
-      if (!user.roles.includes(UserRole.QUANTIFIER)) {
-        user.roles.push(UserRole.QUANTIFIER);
+      if (!user.roles.includes(UserRole.ADMIN)) {
+        user.roles.push(UserRole.ADMIN);
         user.save();
       }
     } else {
       await UserModel.create({
         ethereumAddress: e,
-        roles: [UserRole.QUANTIFIER],
+        roles: [UserRole.ADMIN],
       });
     }
   });
