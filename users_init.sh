2d714200bb584ec01d2f2df903791748db8847b0
Nebojsa Stojanovic <nebojsa.stojanovic0@gmail.com>
2022-02-17 15:46:39 +0100

    changes for mongo env variables

--- users_init.sh
+++ users_init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-# DB_USER is the userName used from applicatoin code to interact with databases and DB_PASSWORD is the password for this user.
+# MONGO_DB_USER is the userName used from applicatoin code to interact with databases and MONGO_DB_PASSWORD is the password for this user.
 # MONGO_INITDB_ROOT_USERNAME & MONGO_INITDB_ROOT_PASSWORD is the config for db admin.
 # admin user is expected to be already created when this script executes. We use it here to authenticate as admin to create
-# DB_USER and databases.
+# MONGO_DB_USER and databases.
 
 echo ">>>>>>> trying to create database and users"
-if [ -n "${MONGO_INITDB_ROOT_USERNAME:-}" ] && [ -n "${MONGO_INITDB_ROOT_PASSWORD:-}" ] && [ -n "${DB_USER:-}" ] && [ -n "${DB_PASSWORD:-}" ]; then
+if [ -n "${MONGO_INITDB_ROOT_USERNAME:-}" ] && [ -n "${MONGO_INITDB_ROOT_PASSWORD:-}" ] && [ -n "${MONGO_DB_USER:-}" ] && [ -n "${DB_PASSWORD:-}" ]; then
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD<<EOF
 use praise_db;
 db.createUser({
-  user:  '$DB_USER',
-  pwd: '$DB_PASSWORD',
+  user:  '$MONGO_DB_USER',
+  pwd: '$MONGO_DB_PASSWORD',
   roles: [{
     role: 'readWrite',
     db: 'praise_db'
@@ -20,6 +20,6 @@ db.createUser({
 });
 EOF
 else
-    echo "MONGO_INITDB_ROOT_USERNAME,MONGO_INITDB_ROOT_PASSWORD, DB_USER and DB_PASSWORD must be provided. Some of these are missing, hence exiting database and user creatioin"
+    echo "MONGO_INITDB_ROOT_USERNAME,MONGO_INITDB_ROOT_PASSWORD, MONGO_DB_USER and MONGO_DB_PASSWORD must be provided. Some of these are missing, hence exiting database and user creatioin"
     exit 403
 fi
