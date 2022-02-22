64db6bc8a648f9067c4d5e3b0cd86a59813c5e1b
Nebojsa Stojanovic <nebojsa.stojanovic0@gmail.com>
2022-02-22 11:12:26 +0100

    transformer changes, checkbox change

--- packages/frontend/src/pages/Settings/SettingsPage.tsx
+++ packages/frontend/src/pages/Settings/SettingsPage.tsx
@@ -145,7 +145,7 @@ const SettingsForm = () => {
               <input
                 id={setting.key}
                 {...input}
-                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
               />
               {apiResponse && (
                 <FieldErrorMessage name="name" apiResponse={apiResponse} />
