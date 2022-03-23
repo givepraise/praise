ab52b69f863381492dbde04bc9bd12075438a822
kristoferlund <kristofer@fmckl.se>
2022-03-23 11:37:40 +0100

    Fix: Ensure top border is rounded

--- packages/frontend/src/components/ScrollableDialog.tsx
+++ packages/frontend/src/components/ScrollableDialog.tsx
@@ -15,10 +15,10 @@ const ScrollableDialog = ({
   return (
     <div className="absolute top-0 left-0 w-full h-full">
       <div className="fixed w-full h-screen">
-        <div className="flex w-full h-full justify-center items-center bg-gray-800 bg-opacity-30">
+        <div className="flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-30">
           <OutsideClickHandler onOutsideClick={onClose} active={open}>
-            <div className="bg-white max-w-xl h-64 z-20">
-              <div className="bg-white rounded pb-16">{children}</div>
+            <div className="z-20 h-64 max-w-xl bg-white rounded">
+              <div className="pb-16 bg-white rounded">{children}</div>
             </div>
           </OutsideClickHandler>
         </div>
