b73e069824410ff97a42666d864c3d774816db31
Vyvy-vi <vyom.j@protonmail.com>
2022-03-25 17:04:23 +0530

    feat: re-add the /activate command

---
+++ packages/discord-bot/src/commands/activate.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import logger from 'jet-logger';
+import { activationHandler } from '../handlers/activate';
+import { Command } from '../interfaces/Command';
+
+export const activate: Command = {
+  data: new SlashCommandBuilder()
+    .setName('activate')
+    .setDescription('Activate your praise account by linking your eth address'),
+
+  async execute(interaction) {
+    try {
+      if (!interaction.isCommand() || interaction.commandName !== 'activate')
+        return;
+      await activationHandler(interaction);
+    } catch (err) {
+      logger.err(err);
+    }
+  },
+};
