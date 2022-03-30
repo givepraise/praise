647a4e2820ba35c07c2a290dce2d3568f0d97d92
Matt Gabrenya <matt@buildyourweb.app>
2022-03-29 20:21:28 -0700

    fix(api): ensure settingValue wrapper correctly fetches both periodsetting or global setting

--- packages/api/src/shared/settings.ts
+++ packages/api/src/shared/settings.ts
@@ -1,17 +1,31 @@
 import { SettingsModel } from '@settings/entities';
+import { PeriodSettingsModel } from '@periodsettings/entities';
 import mongoose from 'mongoose';
 
 export const settingValue = async (
   key: string,
   periodId: mongoose.Schema.Types.ObjectId | undefined = undefined
 ): Promise<string | boolean | number | number[]> => {
-  const setting = await SettingsModel.findOne({
-    key,
-    period: periodId,
-  });
-  if (!setting) {
-    const periodString = periodId ? `period ${periodId.toString()}` : 'global';
-    throw Error(`Setting ${key} does not exist for ${periodString}`);
+  let setting;
+  if (!periodId) {
+    setting = await SettingsModel.findOne({
+      key,
+    });
+
+    if (!setting) {
+      throw Error(`Setting ${key} does not exist`);
+    }
+  } else {
+    setting = await PeriodSettingsModel.findOne({
+      key,
+      period: periodId,
+    });
+    if (!setting) {
+      const periodString = periodId
+        ? `period ${periodId.toString()}`
+        : 'global';
+      throw Error(`periodsetting ${key} does not exist for ${periodString}`);
+    }
   }
 
   return setting.valueRealized;
