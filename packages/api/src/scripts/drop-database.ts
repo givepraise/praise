a06691a921f4b747647e2cba206bac27c05c2760
Matt Gabrenya <matt@buildyourweb.app>
2022-04-01 18:32:30 -0700

    refactor: fix mongodb:clean command - refactor to use api mongoose connection

---
+++ packages/api/src/scripts/drop-database.ts
@@ -0,0 +1,26 @@
+import { PeriodModel } from '@period/entities';
+import { PraiseModel } from '@praise/entities';
+import { UserModel } from '@user/entities';
+import { UserAccountModel } from '@useraccount/entities';
+import { SettingsModel } from '@settings/entities';
+
+import { connectDatabase } from './core';
+
+connectDatabase().then(async (connection) => {
+    try {
+        await Promise.all(
+            [
+                PeriodModel.deleteMany({}),
+                PraiseModel.deleteMany({}),
+                UserModel.deleteMany({}),
+                UserAccountModel.deleteMany({}),
+                SettingsModel.deleteMany({}),
+            ]);
+        console.log('Deleted all collections');
+
+    } catch (err) {
+        console.error(`Failed to delete all collections ${(err as Error).message}`);
+    }
+
+    process.exit();
+});
