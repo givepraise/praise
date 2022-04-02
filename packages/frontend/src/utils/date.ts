6ffeff028afc0850ea4ed306916330c961140f59
Matt Gabrenya <matt@buildyourweb.app>
2022-04-02 14:43:11 -0700

    feat(api): date util funcitons localDateToUtc and utcDateToLocal

--- packages/frontend/src/utils/date.ts
+++ packages/frontend/src/utils/date.ts
@@ -1,4 +1,6 @@
 import { format, isValid } from 'date-fns';
+import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
+import jstz from 'jstz';
 
 export const DATE_FORMAT = 'yyyy-MM-dd';
 export const DATE_FORMAT_LONG = 'yyyy-MM-dd HH:mm';
@@ -13,4 +15,18 @@ export const formatDateLong = (isoDate: string): string => {
 
 export const isValidDate = (inputString: string): boolean => {
   return isValid(new Date(inputString));
+};
+
+export const localDateToUtc = (dateLocal: Date): Date => {
+  const timezone = jstz.determine().name();
+  const dateUtc = zonedTimeToUtc(dateLocal, timezone);
+
+  return dateUtc;
+};
+
+export const utcDateToLocal = (dateUtc: Date): Date => {
+  const timezone = jstz.determine().name();
+  const dateLocal = utcToZonedTime(dateUtc, timezone);
+
+  return dateLocal;
 };
