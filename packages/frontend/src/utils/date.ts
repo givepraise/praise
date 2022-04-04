1f1aed6812a59bc76d41abf7bf1bc456f997d32b
Matt Gabrenya <matt@buildyourweb.app>
2022-04-04 10:28:07 -0900

    refactor(frontend): make formatDate function more generalized

--- packages/frontend/src/utils/date.ts
+++ packages/frontend/src/utils/date.ts
@@ -1,15 +1,21 @@
-import { format, isValid } from 'date-fns';
+import { format, isValid, parse } from 'date-fns';
 import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
 import jstz from 'jstz';
 
 export const DATE_FORMAT = 'yyyy-MM-dd';
 export const DATE_FORMAT_LONG = 'yyyy-MM-dd HH:mm';
 
-export const formatDate = (isoDate: string): string => {
-  return format(new Date(isoDate), DATE_FORMAT);
+export const formatDate = (
+  isoDate: string,
+  pattern: string = DATE_FORMAT
+): string => {
+  return format(new Date(isoDate), pattern);
 };
 
 export const formatDateLong = (isoDate: string): string => {
+  return formatDate(isoDate, DATE_FORMAT_LONG);
+};
+
   return format(new Date(isoDate), DATE_FORMAT_LONG);
 };
 
