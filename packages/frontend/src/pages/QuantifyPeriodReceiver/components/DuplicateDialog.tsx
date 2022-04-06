8702285209bdc33f9f2c02d97dc2d6dda0cf253a
kristoferlund <kristofer@fmckl.se>
2022-04-06 13:02:15 +0200

    Add: Support for period settings

--- packages/frontend/src/pages/QuantifyPeriodReceiver/components/DuplicateDialog.tsx
+++ packages/frontend/src/pages/QuantifyPeriodReceiver/components/DuplicateDialog.tsx
@@ -3,8 +3,9 @@ import { faCalculator, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PraiseDto } from 'api/dist/praise/types';
 import PraiseAutosuggest from './PraiseAutosuggest';
-import { AllSettings, SingleSetting } from '../../../model/settings';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { PeriodPageParams } from '@/model/periods';
+import { useParams } from 'react-router-dom';
+import { usePeriodSettingValueRealized } from '@/model/periodsettings';
 
 interface DuplicateDialogProps {
   onClose(): void;
@@ -19,9 +20,13 @@ const DuplicateDialog = ({
   open = false,
   praise,
 }: DuplicateDialogProps): JSX.Element | null => {
-  const duplicatePraisePercentageSetting = useRecoilValue(
-    SingleSetting('PRAISE_QUANTIFY_DUPLICATE_PRAISE_PERCENTAGE')
-  );
+  const { periodId } = useParams<PeriodPageParams>();
+
+  const duplicatePraisePercentage = usePeriodSettingValueRealized(
+    periodId,
+    'PRAISE_QUANTIFY_DUPLICATE_PRAISE_PERCENTAGE'
+  ) as number;
+
   if (!praise) return null;
 
   return (
@@ -39,11 +44,12 @@ const DuplicateDialog = ({
           <h2 className="text-center mb-7">
             Mark praise #{praise._id.slice(-4)} as duplicate
           </h2>
-          <p className="text-center mb-7">
-            Duplicate praise are given a score that is{' '}
-            {duplicatePraisePercentageSetting?.value}% of the original
-            quantification.
-          </p>
+          {duplicatePraisePercentage && (
+            <p className="text-center mb-7">
+              Duplicate praise are given a score that is{' '}
+              {duplicatePraisePercentage * 100}% of the original quantification.
+            </p>
+          )}
 
           <div className="flex justify-center">
             <PraiseAutosuggest
