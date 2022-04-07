d576f9172876eafffed41a07f9911203da15f7bc
kristoferlund <kristofer@fmckl.se>
2022-04-07 10:08:44 +0200

    Fix: Removed unused dependencies

--- packages/frontend/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-// eslint-disable-next-line no-unused-vars
-import { startDevServer } from '@cypress/webpack-dev-server';
-require('dotenv').config();
-
-module.exports = (on, config) => {
-  return config;
-};
