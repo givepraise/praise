7d7622b5150d1c66b84c3c349e57f5f1382b643e
kristoferlund <kristofer@fmckl.se>
2022-04-10 21:17:19 +0200

    Remove dependency dotenv-expand

--- packages/api/src/loadEnv.ts
+++ packages/api/src/loadEnv.ts
@@ -1,5 +1,4 @@
 import * as dotenv from 'dotenv';
-import * as dotenvExpand from 'dotenv-expand';
 import logger from 'jet-logger';
 import path from 'path';
 
@@ -10,7 +9,6 @@ if (env.error) {
   logger.err(env.error.message);
   throw env.error;
 }
-dotenvExpand.expand(env);
 env = dotenv.config({
   path: path.join(__dirname, '..', '/.env'),
 });
