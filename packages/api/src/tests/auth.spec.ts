d10533e45e9237aaa68b13f746326d7626704fa8
Matt Gabrenya <matt@buildyourweb.app>
2022-04-14 16:53:09 -0400

    test(api): verify /api/auth/nonce returns json response

--- packages/api/src/tests/auth.spec.ts
+++ packages/api/src/tests/auth.spec.ts
@@ -1,9 +1,11 @@
 describe('GET /api/auth/nonce', () => {
-  it('200 response', function () {
+  it('200 response with json body', function () {
     const ETHEREUM_ADDRESS = '0x1234';
 
     return this.client
       .get(`/api/auth/nonce?ethereumAddress=${ETHEREUM_ADDRESS}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .expect(200);
   });
   it('404 response when missing ethereumAddress', function () {
