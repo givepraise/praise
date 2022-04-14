cbfaaa3bdc4b0210fccb9fb615b1983a4c32ab5c
Matt Gabrenya <matt@buildyourweb.app>
2022-04-14 16:49:49 -0400

    chore(api): fix typo

--- packages/api/src/tests/example.spec.ts
+++ packages/api/src/tests/example.spec.ts
@@ -3,7 +3,7 @@ import supertest from 'supertest';
 const request = supertest('https://airportgap.dev-tester.com/api');
 
 describe('EXAMPLE TEST', () => {
-  it('404 upon calling random 3rd party api', () => {
+  it('401 upon calling random 3rd party api', () => {
     return request
       .post('/favorites')
       .send({
