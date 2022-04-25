9cd0afbda6cb74a008a8f44c80aa5053767a4761
kristoferlund <kristofer@fmckl.se>
2022-04-25 15:16:21 +0200

    Lint error

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,5 +5,5 @@ import { app } from './server';
 // Start the server
 const port = process.env.API_PORT;
 app.listen(port, () => {
-  logger.info(`Express server started on port: ${port}`);
+  logger.info(`Express server started on port: ${port as string}`);
 });
