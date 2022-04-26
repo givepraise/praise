961699b59d023f44709d3e480bf5cd9814860c90
Matt Gabrenya <matt@buildyourweb.app>
2022-04-26 14:17:52 -0400

    chore(api): remove 'export' of function that is only used locally

--- packages/api/src/praise/utils/score.ts
+++ packages/api/src/praise/utils/score.ts
@@ -63,7 +63,7 @@ export const calculatePraiseScore = async (
   );
 };
 
-export const calculateDuplicateScore = async (
+const calculateDuplicateScore = async (
   quantification: Quantification,
   periodId: mongoose.Schema.Types.ObjectId
 ): Promise<number> => {
