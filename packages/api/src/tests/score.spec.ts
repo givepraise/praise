1f7e16b29087249bbeac648b29ba0b61f89ff403
Matt Gabrenya <matt@buildyourweb.app>
2022-04-29 22:51:16 -0400

    test(api): unit test for 'calculateReceiverCompositeScore()'

--- packages/api/src/tests/score.spec.ts
+++ packages/api/src/tests/score.spec.ts
@@ -1,17 +1,24 @@
 import {
   calculateQuantificationScore,
   calculateQuantificationsCompositeScore,
+  calculateReceiverCompositeScore,
 } from '@praise/utils/score';
 import {
   seedUser,
   seedPraise,
   seedQuantification,
   seedPeriod,
+  seedUserAccount,
 } from '../pre-start/seed';
 import { expect } from 'chai';
 import { PeriodModel } from '@period/entities';
+import { PeriodDetailsReceiver } from '@period/types';
+import { PraiseModel } from '@praise/entities';
 import { settingValue } from '@shared/settings';
 import { logger } from 'ethers';
+import { sum } from 'lodash';
+import { getPeriodDateRangeQuery } from '@period/utils';
+import faker from 'faker';
 
 describe('calculateQuantificationScore', () => {
   beforeEach(async () => {
@@ -336,5 +343,153 @@ describe('calculateQuantificationsCompositeScore', () => {
     );
 
     expect(score).equals(Math.floor((10 + 30 + 50 + 70) / 4));
+  });
+});
+
+describe('calculateReceiverCompositeScore', () => {
+  beforeEach(async () => {
+    await PeriodModel.deleteMany({});
+    await PraiseModel.deleteMany({});
+  });
+
+  it('receiver composite score is sum of all quantification composite scores', async () => {
+    const user = await seedUser();
+    const receiver = await seedUserAccount(user);
+
+    const praise = await seedPraise({
+      receiver: receiver._id,
+      createdAt: faker.date.soon(3),
+    });
+
+    const quantifier = await seedUser();
+    await seedQuantification(praise, quantifier, {
+      dismissed: false,
+      score: 10,
+    });
+
+    const quantifier2 = await seedUser();
+    await seedQuantification(praise, quantifier2, {
+      dismissed: false,
+      score: 30,
+    });
+
+    const quantifier3 = await seedUser();
+    await seedQuantification(praise, quantifier3, {
+      dismissed: false,
+      score: 50,
+    });
+
+    const quantifier4 = await seedUser();
+    await seedQuantification(praise, quantifier4, {
+      dismissed: false,
+      score: 70,
+    });
+
+    const praise2 = await seedPraise({
+      receiver: receiver._id,
+      createdAt: faker.date.soon(4)
+    });
+    await seedQuantification(praise2, quantifier, {
+      dismissed: false,
+      score: 10,
+    });
+    await seedQuantification(praise2, quantifier2, {
+      dismissed: false,
+      score: 30,
+    });
+    await seedQuantification(praise2, quantifier3, {
+      dismissed: false,
+      score: 50,
+    });
+    await seedQuantification(praise2, quantifier4, {
+      dismissed: false,
+      score: 70,
+    });
+
+    const praise3 = await seedPraise({
+      receiver: receiver._id,
+      createdAt: faker.date.soon(5)
+    });
+    await seedQuantification(praise3, quantifier, {
+      dismissed: false,
+      score: 10,
+    });
+    await seedQuantification(praise3, quantifier2, {
+      dismissed: false,
+      score: 30,
+    });
+    await seedQuantification(praise3, quantifier3, {
+      dismissed: false,
+      score: 50,
+    });
+    await seedQuantification(praise3, quantifier4, {
+      dismissed: false,
+      score: 70,
+    });
+
+    const praise4 = await seedPraise({
+      receiver: receiver._id,
+      createdAt: faker.date.soon(6),
+    });
+    await seedQuantification(praise4, quantifier, {
+      dismissed: false,
+      score: 10,
+    });
+    await seedQuantification(praise4, quantifier2, {
+      dismissed: false,
+      score: 30,
+    });
+    await seedQuantification(praise4, quantifier3, {
+      dismissed: false,
+      score: 50,
+    });
+    await seedQuantification(praise4, quantifier4, {
+      dismissed: false,
+      score: 70,
+    });
+
+    const period = await seedPeriod({
+      endDate: faker.date.soon(7),
+    });
+
+    const dateRangeQuery = await getPeriodDateRangeQuery(period);
+    const periodDetailsReceivers: PeriodDetailsReceiver[] =
+      await PraiseModel.aggregate([
+        {
+          $match: {
+            createdAt: dateRangeQuery,
+          },
+        },
+        {
+          $lookup: {
+            from: 'useraccounts',
+            localField: 'receiver',
+            foreignField: '_id',
+            as: 'userAccounts',
+          },
+        },
+        {
+          $group: {
+            _id: '$receiver',
+            praiseCount: { $count: {} },
+            quantifications: {
+              $push: '$quantifications',
+            },
+            userAccounts: { $first: '$userAccounts' },
+          },
+        },
+      ]);
+
+    const score = await calculateReceiverCompositeScore(
+      periodDetailsReceivers[0]
+    );
+
+    const compositeScores = await Promise.all(
+      [praise, praise2, praise3, praise4].map((p) =>
+        calculateQuantificationsCompositeScore(p.quantifications)
+      )
+    );
+
+    expect(score).equals(sum(compositeScores));
   });
 });
