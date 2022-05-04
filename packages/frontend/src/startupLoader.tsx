50c31bf3bc11fbcf9c404eca051d6424a811f6d6
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-05-04 14:50:18 -0300

    style: make the loading component centralized for all page sizes

--- packages/frontend/src/startupLoader.tsx
+++ packages/frontend/src/startupLoader.tsx
@@ -18,7 +18,7 @@ export const LoadScreen: React.FC = (): ReactElement | null => {
   return (
     <div className="fixed top-0 left-0 z-50 block w-full h-full bg-white opacity-75">
       <span
-        className="relative block w-0 h-0 mx-auto my-0 top-1/2"
+        className="relative w-0 h-0 top-1/2 mx-auto flex justify-center align-center"
         style={{ top: '50%' }}
       >
         <FontAwesomeIcon icon={faPrayingHands} size="5x" spin />
