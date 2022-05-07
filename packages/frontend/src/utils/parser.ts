b152093f83b5da66eb67e7a65bca41e85a352145
Nebojsa Stojanovic <nebojsa.stojanovic0@gmail.com>
2022-05-07 21:58:41 +0200

    Added formatter for code in praise reason

--- packages/frontend/src/utils/parser.ts
+++ packages/frontend/src/utils/parser.ts
@@ -1,5 +1,7 @@
 export const markdownParser = (text: string): string => {
   const toHTML = text
+    .replace(/```([^_]*)```/gim, '<pre><code>$1</code></pre>') // multiline code
+    .replace(/`([^_]*)`/gim, '<code>$1</code>') // inline code
     .replace(/__([^_]*)__/gim, '<u>$1</u>') // underscore text
     .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
     .replace(/_([^_]*)_/gim, '<i>$1</i>') // italic text
