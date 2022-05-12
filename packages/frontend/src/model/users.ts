328be65158fc7eb6e7d3841e40ed2e1a97203fd5
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-05-12 16:20:38 -0300

    feat: added selector for all admins and for all forwarders users

--- packages/frontend/src/model/users.ts
+++ packages/frontend/src/model/users.ts
@@ -44,12 +44,34 @@ export const AllUsers = atom<UserDto[] | undefined>({
   default: undefined,
 });
 
+export const AllAdminUsers = selector({
+  key: 'AllAdminUsers',
+  get: ({ get }) => {
+    const users = get(AllUsers);
+    if (users) {
+      return users.filter((user) => user.roles.includes(UserRole.ADMIN));
+    }
+    return undefined;
+  },
+});
+
 export const AllQuantifierUsers = selector({
   key: 'AllQuantifierUsers',
   get: ({ get }) => {
     const users = get(AllUsers);
     if (users) {
       return users.filter((user) => user.roles.includes(UserRole.QUANTIFIER));
+    }
+    return undefined;
+  },
+});
+
+export const AllForwarderUsers = selector({
+  key: 'AllForwarderUsers',
+  get: ({ get }) => {
+    const users = get(AllUsers);
+    if (users) {
+      return users.filter((user) => user.roles.includes(UserRole.FORWARDER));
     }
     return undefined;
   },
@@ -72,19 +94,23 @@ export const useAllUsersQuery = (): AxiosResponse<unknown> => {
   return allUsersQueryResponse;
 };
 
-type SingleUserParams = {
+/**
+ * Types for `useParams()`
+ */
+export type SingleUserParams = {
   userId: string | undefined;
 };
+
 export const SingleUser = selectorFamily({
   key: 'SingleUser',
   get:
     (params: SingleUserParams) =>
-    ({ get }): UserDto | undefined => {
-      const { userId } = params;
-      const allUsers = get(AllUsers);
-      if (!allUsers) return undefined;
-      return allUsers.filter((user) => user._id === userId)[0];
-    },
+      ({ get }): UserDto | undefined => {
+        const { userId } = params;
+        const allUsers = get(AllUsers);
+        if (!allUsers) return undefined;
+        return allUsers.filter((user) => user._id === userId)[0];
+      },
 });
 
 type SingleUserByReceiverIdParams = {
@@ -94,15 +120,15 @@ export const SingleUserByReceiverId = selectorFamily({
   key: 'SingleUserByReceiverId',
   get:
     (params: SingleUserByReceiverIdParams) =>
-    ({ get }): UserDto | undefined => {
-      const { receiverId } = params;
-      const allUsers = get(AllUsers);
-      if (!allUsers) return undefined;
-      return allUsers.find((user) => {
-        if (!user.accounts) return false;
-        return user.accounts.find((account) => account._id === receiverId);
-      });
-    },
+      ({ get }): UserDto | undefined => {
+        const { receiverId } = params;
+        const allUsers = get(AllUsers);
+        if (!allUsers) return undefined;
+        return allUsers.find((user) => {
+          if (!user.accounts) return false;
+          return user.accounts.find((account) => account._id === receiverId);
+        });
+      },
 });
 
 const stringToNumber = (s: string): number => {
@@ -121,22 +147,22 @@ export const PseudonymForUser = selectorFamily({
   key: 'PseudonymForUser',
   get:
     (params: PseudonymForUserParams) =>
-    ({ get }): string => {
-      const { periodId, userId } = params;
-      const allPeriods = get(AllPeriods);
-      if (!allPeriods) return 'Loading…';
-      const periodIndex = allPeriods.findIndex((p) => p._id === periodId);
+      ({ get }): string => {
+        const { periodId, userId } = params;
+        const allPeriods = get(AllPeriods);
+        if (!allPeriods) return 'Loading…';
+        const periodIndex = allPeriods.findIndex((p) => p._id === periodId);
 
-      if (userId && periodIndex > -1) {
-        const u = stringToNumber(userId);
-        const p = stringToNumber(periodId);
-        const n = pseudonymNouns[(u + p) % pseudonymNouns.length];
-        const a = psudonymAdjectives[(u + p) % psudonymAdjectives.length];
-        return `${a} ${n}`;
-      }
+        if (userId && periodIndex > -1) {
+          const u = stringToNumber(userId);
+          const p = stringToNumber(periodId);
+          const n = pseudonymNouns[(u + p) % pseudonymNouns.length];
+          const a = psudonymAdjectives[(u + p) % psudonymAdjectives.length];
+          return `${a} ${n}`;
+        }
 
-      return 'Unknown user';
-    },
+        return 'Unknown user';
+      },
 });
 
 export const AddUserRoleApiResponse = atom<
