5189282a9d5d11bab6faa0334c0a509c839253cc
Matt Gabrenya <matt@buildyourweb.app>
2022-05-14 13:27:41 -0400

    feat(frontend): add optional prop 'title' to InlineLabel

--- packages/frontend/src/components/InlineLabel.tsx
+++ packages/frontend/src/components/InlineLabel.tsx
@@ -2,6 +2,7 @@ import { classNames } from '../utils';
 
 interface InlineLabelProps {
   text: string;
+  title?: string;
   button?: JSX.Element;
   className?: string;
   onClick?: () => void;
@@ -9,6 +10,7 @@ interface InlineLabelProps {
 
 export const InlineLabel = ({
   text,
+  title,
   button,
   className,
   onClick,
@@ -21,6 +23,7 @@ export const InlineLabel = ({
         'h-6 pl-1 pr-1 mr-1 text-xs text-white no-underline bg-gray-800 py-[1px] rounded',
         onClick ? 'cursor-pointer' : ''
       )}
+      title={title}
     >
       {text}
       {button && button}
