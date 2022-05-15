1ef160df62001899da0fe5ef145e361a0cce15fb
Matt Gabrenya <matt@buildyourweb.app>
2022-05-15 12:26:00 -0400

    style(frontend): lighten active NavItem backgroun color

--- packages/frontend/src/navigation/NavItem.tsx
+++ packages/frontend/src/navigation/NavItem.tsx
@@ -13,8 +13,8 @@ const NavItem = ({ icon, description, to }: NavProps): JSX.Element => {
     <NavLink
       to={to}
       className={(isActive): string =>
-        `relative px-4 py-1 cursor-pointer mr-[1px] no-underline flex items-center${
-          isActive ? ' bg-gray-800 text-white' : ' hover:bg-gray-100 text-black'
+        `relative px-4 py-1 cursor-pointer mr-[1px] no-underline flex items-center text-black ${
+          isActive ? ' bg-gray-200' : ' hover:bg-gray-100'
         }`
       }
       id={to.substring(1) + '-nav-button'}
