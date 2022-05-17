322da3cfdb5f4f7b72b3d24055e072f477020d59
Matt Gabrenya <matt@buildyourweb.app>
2022-05-17 14:53:30 -0400

    feat(frontend): group settings form into sub-pages by setting.group

--- packages/frontend/src/pages/Settings/SettingsPage.tsx
+++ packages/frontend/src/pages/Settings/SettingsPage.tsx
@@ -1,23 +1,79 @@
+import { SettingGroup } from 'api/dist/settings/types';
 import BreadCrumb from '@/components/BreadCrumb';
 import { faCogs } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import SettingsForm from '../../components/settings/SettingsForm';
 import { AllSettings, useSetSetting } from '@/model/settings';
 import { useRecoilValue } from 'recoil';
+import NavItem from '@/navigation/NavItem';
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 
-const SettingsPage = (): JSX.Element => {
+const SettingsPage = (): JSX.Element | null => {
   const settings = useRecoilValue(AllSettings);
   const { setSetting } = useSetSetting();
+  const { path, url } = useRouteMatch();
+
+  if (!settings) return null;
+
+  const applicationSettings = settings.filter(
+    (s) => s.group === SettingGroup.APPLICATION
+  );
+  const periodDefaultSettings = settings.filter(
+    (s) => s.group === SettingGroup.PERIOD_DEFAULT
+  );
+  const discordSettings = settings.filter(
+    (s) => s.group === SettingGroup.DISCORD
+  );
 
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-3xl mx-auto">
       <BreadCrumb name="Settings" icon={faCogs} />
 
-      <div className="w-full praise-box">
-        <h2 className="mb-2">Settings</h2>
-        <React.Suspense fallback="Loading…">
-          <SettingsForm settings={settings} setSetting={setSetting} />
-        </React.Suspense>
+      <div className="flex space-x-4">
+        <div>
+          <div className="w-40 py-5 mb-5 break-words border rounded-lg shadow-sm bg-gray-50">
+            <nav>
+              <NavItem to={`${url}/application`} description="Application" />
+              <NavItem to={`${url}/period`} description="Period Defaults" />
+              <NavItem to={`${url}/discord`} description="Discord" />
+            </nav>
+          </div>
+        </div>
+
+        <div className="w-full max-w-3xl praise-box">
+          <Switch>
+            <Route path={`${path}/application`}>
+              <h2 className="mb-2">Application Settings</h2>
+              <React.Suspense fallback="Loading…">
+                <SettingsForm
+                  settings={applicationSettings}
+                  setSetting={setSetting}
+                />
+              </React.Suspense>
+            </Route>
+            <Route path={`${path}/period`}>
+              <h2 className="mb-2">Period Default Settings</h2>
+              <React.Suspense fallback="Loading…">
+                <SettingsForm
+                  settings={periodDefaultSettings}
+                  setSetting={setSetting}
+                />
+              </React.Suspense>
+            </Route>
+            <Route path={`${path}/discord`}>
+              <h2 className="mb-2">Discord Settings</h2>
+              <React.Suspense fallback="Loading…">
+                <SettingsForm
+                  settings={discordSettings}
+                  setSetting={setSetting}
+                />
+              </React.Suspense>
+            </Route>
+            <Route path={`${path}`}>
+              <Redirect to={`${url}/application`} />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
   );
