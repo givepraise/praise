28e2467b6124543b0329ef6e638c6ef2b15e80d4
Pedro Kretzschmar <pedrokretzschmar@Pedros-MacBook-Pro.local>
2022-06-06 16:17:01 -0300

    feat: added csv download button

--- packages/frontend/src/pages/Users/components/UsersStatistics.tsx
+++ packages/frontend/src/pages/Users/components/UsersStatistics.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useRecoilValue } from 'recoil';
+import { CSVLink } from 'react-csv';
 import {
   AllAdminUsers,
   AllForwarderUsers,
@@ -14,14 +14,26 @@ const UsersStatistics = (): JSX.Element => {
   const allUsers = useRecoilValue(AllUsers);
 
   return (
-    <div className="praise-box">
-      <div className="mb-4">
-        <span className="font-bold text-xl">User statistics</span>
+    <div className="praise-box flex justify-between">
+      {console.log(allUsers)}
+      <div className="max-w-1/2">
+        <div className="mb-4">
+          <span className="font-bold text-xl">User statistics</span>
+        </div>
+        <div>Activated users: {allUsers?.length}</div>
+        <div>Admins: {allAdminUsers?.length}</div>
+        <div>Forwarders: {allForwarderUsers?.length}</div>
+        <div>Quantifiers: {allQuantifierUsers?.length}</div>
       </div>
-      <div>Activated users: {allUsers?.length}</div>
-      <div>Admins: {allAdminUsers?.length}</div>
-      <div>Forwarders: {allForwarderUsers?.length}</div>
-      <div>Quantifiers: {allQuantifierUsers?.length}</div>
+      <CSVLink
+        className="mt-auto praise-button-small h-fit bg-black px-2 py-1 rounded"
+        data={allUsers}
+        separator={';'}
+      >
+        <span className="font-medium text-white uppercase no-underline">
+          download csv
+        </span>
+      </CSVLink>
     </div>
   );
 };
