e92202e30263077403dc19d0758586c2114d7d9b
Matt Gabrenya <matt@buildyourweb.app>
2022-06-07 19:37:24 -0400

    fix(api): include adding PeriodSetting in migration

--- packages/api/src/database/migrations/13_settings_manual_quantifier_assigment.ts
+++ packages/api/src/database/migrations/13_settings_manual_quantifier_assigment.ts
@@ -1,35 +1,34 @@
-import { SettingGroup } from '../../settings/types';
-import { SettingsModel } from '../../settings/entities';
+import { SettingGroup } from '@settings/types';
+import { SettingsModel } from '@settings/entities';
+import { PeriodSettingsModel } from '@periodsettings/entities';
+import { PeriodModel } from '@period/entities';
+import { PeriodDocument } from '@period/types';
 
-const settings = [
-  {
-    key: 'PRAISE_QUANTIFIERS_ASSIGN_ALL',
-    value: false,
-    type: 'Boolean',
-    label: 'Assign praise to all quantifiers',
-    description:
-      'Evenly assign praise among all quantifiers. If enabled, the setting "Quantifiers Per Praise" will be ignored',
-    group: SettingGroup.PERIOD_DEFAULT,
-  },
-];
+const newSetting = {
+  key: 'PRAISE_QUANTIFIERS_ASSIGN_ALL',
+  value: false,
+  type: 'Boolean',
+  label: 'Assign praise to all quantifiers',
+  description:
+    'Evenly assign praise among all quantifiers. If enabled, the setting "Quantifiers Per Praise" will be ignored',
+  group: SettingGroup.PERIOD_DEFAULT,
+};
 
 const up = async (): Promise<void> => {
-  const settingUpdates = settings.map((s) => ({
-    updateOne: {
-      filter: { key: s.key },
+  await SettingsModel.create(newSetting);
 
-      // Insert setting if not found, otherwise continue
-      update: { $setOnInsert: { ...s } },
-      upsert: true,
-    },
+  const periods = await PeriodModel.find({});
+  const periodsettings = periods.map((p: PeriodDocument) => ({
+    ...newSetting,
+    period: p._id,
   }));
 
-  await SettingsModel.bulkWrite(settingUpdates);
+  await PeriodSettingsModel.insertMany(periodsettings);
 };
 
 const down = async (): Promise<void> => {
-  const allKeys = settings.map((s) => s.key);
-  await SettingsModel.deleteMany({ key: { $in: allKeys } });
+  await SettingsModel.deleteMany({ key: newSetting.key });
+  await PeriodSettingsModel.deleteMany({ key: newSetting.key });
 };
 
 export { up, down };
