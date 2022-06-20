07244091dc349bdad45bf135406e7745dc2efe4e
kristoferlund <kristofer@fmckl.se>
2022-06-20 10:50:24 +0200

    Remove 'null' return

--- packages/frontend/src/model/users.ts
+++ packages/frontend/src/model/users.ts
@@ -149,7 +149,7 @@ export const PseudonymForUser = selectorFamily({
     ({ get }): string => {
       const { periodId, userId } = params;
       const allPeriods = get(AllPeriods);
-      if (!allPeriods) return 'null';
+      if (!allPeriods) return 'Loading…';
       const periodIndex = allPeriods.findIndex((p) => p._id === periodId);
 
       if (userId && periodIndex > -1) {
