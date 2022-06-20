a34052a3e6da1a47cf75d662c4664adad64ecdcb
Matt Gabrenya <matt@buildyourweb.app>
2022-06-19 20:18:29 -0400

    test(api): tests for period /assignQuantifiers endpoint when PRAISE_QUANTIFIERS_ASSIGN_ALL=true

--- packages/api/src/tests/period.assignment.spec.ts
+++ packages/api/src/tests/period.assignment.spec.ts
@@ -23,7 +23,7 @@ describe('PATCH /api/admin/periods/:periodId/assignQuantifiers', () => {
     await PeriodSettingsModel.deleteMany({});
   });
 
-  it('200 response with json body containing updated period', async function () {
+  it('200 response with json body containing assignments with PRAISE_QUANTIFIERS_ASSIGN_ALL=false', async function () {
     const wallet = Wallet.createRandom();
     await seedUser({
       ethereumAddress: wallet.address,
@@ -39,22 +39,196 @@ describe('PATCH /api/admin/periods/:periodId/assignQuantifiers', () => {
       a._id.toString().localeCompare(b._id.toString())
     );
 
-    await seedPraise({ receiver: receiver1._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver1._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver1._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver1._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver1._id, createdAt: faker.date.past() });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
 
-    await seedPraise({ receiver: receiver2._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver2._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver2._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver2._id, createdAt: faker.date.past() });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
 
-    await seedPraise({ receiver: receiver3._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver3._id, createdAt: faker.date.past() });
-    await seedPraise({ receiver: receiver3._id, createdAt: faker.date.past() });
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
 
     const period = await seedPeriod();
+
+    await PeriodSettingsModel.updateOne(
+      {
+        period: period._id,
+        key: 'PRAISE_QUANTIFIERS_ASSIGN_ALL',
+      },
+      { $set: { value: false } }
+    );
+
+    await seedUser({
+      roles: ['USER', 'QUANTIFIER'],
+    });
+    await seedUser({
+      roles: ['USER', 'QUANTIFIER'],
+    });
+    await seedUser({
+      roles: ['USER', 'QUANTIFIER'],
+    });
+    await seedUser({
+      roles: ['USER', 'QUANTIFIER'],
+    });
+
+    const response = await this.client
+      .patch(
+        `/api/admin/periods/${
+          period._id.toString() as string
+        }/assignQuantifiers`
+      )
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(response.body._id).to.equal(period._id.toString());
+    expect(response.body.status).to.equal('QUANTIFY');
+
+    expect(response.body.receivers).to.have.length(3);
+    expect(response.body.receivers[0]._id).to.equal(
+      receiversSorted[0]._id.toString()
+    );
+    expect(response.body.receivers[0].praiseCount).to.equal(5);
+
+    expect(response.body.receivers[1]._id).to.equal(
+      receiversSorted[1]._id.toString()
+    );
+    expect(response.body.receivers[1].praiseCount).to.equal(4);
+
+    expect(response.body.receivers[2]._id).to.equal(
+      receiversSorted[2]._id.toString()
+    );
+    expect(response.body.receivers[2].praiseCount).to.equal(3);
+
+    expect(response.body.quantifiers).to.have.length(3);
+    expect(response.body.quantifiers[0].praiseCount).to.equal(12);
+    expect(response.body.quantifiers[1].praiseCount).to.equal(12);
+    expect(response.body.quantifiers[2].praiseCount).to.equal(12);
+
+    expect(response.body.quantifiers[0].finishedCount).to.equal(0);
+    expect(response.body.quantifiers[1].finishedCount).to.equal(0);
+    expect(response.body.quantifiers[2].finishedCount).to.equal(0);
+  });
+
+  it('200 response with json body containing assignments with PRAISE_QUANTIFIERS_ASSIGN_ALL=true', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+      roles: ['USER', 'ADMIN'],
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const receiver1 = await seedUserAccount();
+    const receiver2 = await seedUserAccount();
+    const receiver3 = await seedUserAccount();
+
+    const receiversSorted = [receiver1, receiver2, receiver3].sort((a, b) =>
+      a._id.toString().localeCompare(b._id.toString())
+    );
+
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver1._id,
+      createdAt: faker.date.past(),
+    });
+
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver2._id,
+      createdAt: faker.date.past(),
+    });
+
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
+    await seedPraise({
+      receiver: receiver3._id,
+      createdAt: faker.date.past(),
+    });
+
+    const period = await seedPeriod();
+
+    await PeriodSettingsModel.updateOne(
+      {
+        period: period._id,
+        key: 'PRAISE_QUANTIFIERS_ASSIGN_ALL',
+      },
+      { $set: { value: true } }
+    );
 
     await seedUser({
       roles: ['USER', 'QUANTIFIER'],
