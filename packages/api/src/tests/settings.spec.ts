236a776fc09d0475633dd33f645c88c2b3fa8989
Matt Gabrenya <matt@buildyourweb.app>
2022-06-22 16:38:39 -0400

    test(api): tests for settings conversion of value to valueRealized

--- packages/api/src/tests/settings.spec.ts
+++ packages/api/src/tests/settings.spec.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { loginUser } from './utils';
 import { faker } from '@faker-js/faker';
 import { SettingsModel } from '@settings/entities';
-import logger from 'jet-logger';
+import { URL } from 'url';
 
 describe('GET /api/settings/all', () => {
   it('200 response with json body containing list of settings', async function () {
@@ -232,5 +232,125 @@ describe('PATCH /api/admin/settings/:id/set', () => {
       .send(FORM_DATA)
       .expect('Content-Type', /json/)
       .expect(401);
+  });
+});
+
+describe('setting.valueRealized conversions', () => {
+  it('setting.type "Integer" converted to integer number', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const setting = await seedSetting({
+      type: 'Integer',
+      value: 10,
+    });
+
+    const response = await this.client
+      .get(`/api/settings/${setting?._id.toString() as string}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(typeof response.body.valueRealized).to.equal('number');
+    expect(response.body.valueRealized % 1).to.equal(0);
+  });
+
+  it('setting.type "Float" converted to float number', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const setting = await seedSetting({
+      type: 'Float',
+      value: 10.55,
+    });
+
+    const response = await this.client
+      .get(`/api/settings/${setting?._id.toString() as string}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(typeof response.body.valueRealized).to.equal('number');
+    expect(response.body.valueRealized % 1).to.be.greaterThan(0);
+  });
+
+  it('setting.type "Boolean" converted to boolean', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const setting = await seedSetting({
+      type: 'Boolean',
+      value: false,
+    });
+
+    const response = await this.client
+      .get(`/api/settings/${setting?._id.toString() as string}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(typeof response.body.valueRealized).to.equal('boolean');
+    expect(response.body.valueRealized).to.equal(false);
+  });
+
+  it('setting.type "IntegerList" converted to number[] of integers', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const setting = await seedSetting({
+      type: 'IntegerList',
+      value: '1, 2, 3',
+    });
+
+    const response = await this.client
+      .get(`/api/settings/${setting?._id.toString() as string}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(Array.isArray(response.body.valueRealized)).to.be.true;
+    expect(typeof response.body.valueRealized[0]).to.equal('number');
+    expect(response.body.valueRealized[0] % 1).to.equal(0);
+  });
+
+  it('setting.type "Image" converted to string uri', async function () {
+    const wallet = Wallet.createRandom();
+    await seedUser({
+      ethereumAddress: wallet.address,
+    });
+    const { accessToken } = await loginUser(wallet, this.client);
+
+    const setting = await seedSetting({
+      type: 'Image',
+      value: 'file.png',
+    });
+
+    const response = await this.client
+      .get(`/api/settings/${setting?._id.toString() as string}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(typeof response.body.valueRealized[0]).to.equal('string');
+    expect(response.body.valueRealized).to.include(process.env.SERVER_URL);
+    expect(response.body.valueRealized).to.include(setting.value);
+    expect(() => new URL(response.body.valueRealized)).to.not.throw();
   });
 });
