7f8e0ee2093649ee7b899f7b677b369d88f09f5b
kristoferlund <kristofer@fmckl.se>
2022-06-27 21:48:20 +0200

    Upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,3 @@
 docker compose -f ./docker-compose.production.yml pull
+docker compose -f ./docker-compose.production.yml down
 docker compose -f ./docker-compose.production.yml up -d --remove-orphans
