f9d9d60fa9addd0463ddb858f80b08e9073eb1d7
kristoferlund <kristofer@fmckl.se>
2022-08-17 09:27:31 +0200

    Add period analytics subpage

---
+++ packages/frontend/src/pages/Periods/Details/components/Analytics.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from 'react';
+
+import { LoadPlaceholder } from '@/components/analytics/LoadPlaceholder';
+import { Top10Praise } from './analytics/Top10Praise';
+import { TopReceiversByScore } from './analytics/TopReceiversByScore';
+
+import 'react-vis/dist/style.css';
+
+const Analytics = (): JSX.Element => {
+  return (
+    <div className="flex flex-col gap-2 px-5">
+      <span className="text-xl font-bold">Top 10 praise</span>
+      <span>The ten most highly scored praise this period.</span>
+      <Suspense fallback={<LoadPlaceholder height={800} />}>
+        <Top10Praise />
+      </Suspense>
+
+      <span className="text-xl font-bold">Top receivers (by score)</span>
+      <Suspense fallback={<LoadPlaceholder height={600} />}>
+        <TopReceiversByScore />
+      </Suspense>
+    </div>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Analytics;
