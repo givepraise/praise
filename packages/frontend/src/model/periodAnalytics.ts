5ad241f416351eb7d1db22e8bb107c02adf27acb
kristoferlund <kristofer@fmckl.se>
2022-08-19 12:48:31 +0200

    Add a period quantifier summary page

--- packages/frontend/src/model/periodAnalytics.ts
+++ packages/frontend/src/model/periodAnalytics.ts
@@ -26,6 +26,35 @@ export const PeriodTop10Praise = selectorFamily({
     },
 });
 
+export interface PraiseDetailsStats extends PraiseDetailsDto {
+  maxScore: number;
+  minScore: number;
+  scoreSpread: number;
+}
+
+export const PeriodPraiseOutliers = selectorFamily({
+  key: 'PeriodPraiseOutliers',
+  get:
+    (periodId: string | undefined) =>
+    ({ get }): PraiseDetailsStats[] | undefined => {
+      if (!periodId) return undefined;
+      const praise = get(AllPeriodPraise(periodId));
+      if (!praise) return undefined;
+      return praise.map((p) => {
+        let quantified = p.quantifications.map((q) => q.score);
+        quantified = [...quantified].filter(Number);
+        const maxScore = Math.max(...quantified);
+        const minScore = Math.min(...quantified);
+        return {
+          ...p,
+          maxScore,
+          minScore,
+          scoreSpread: maxScore - minScore,
+        };
+      });
+    },
+});
+
 interface PeriodStats {
   totalPraise: number;
   totalPraiseScoreRealized: number;
@@ -62,6 +91,83 @@ export const AllPeriodQuantifications = selectorFamily({
         }
         return acc;
       }, []);
+    },
+});
+
+export const AllPeriodQuantificationsGroupedByQuantifier = selectorFamily({
+  key: 'AllPeriodQuantificationsGroupedByQuantifier',
+  get:
+    (periodId: string | undefined) =>
+    ({ get }): Map<string, QuantificationDto[]> | undefined => {
+      if (!periodId) return undefined;
+      const quantifications = get(AllPeriodQuantifications(periodId));
+      if (!quantifications) return undefined;
+      return quantifications.reduce(
+        (acc: Map<string, QuantificationDto[]>, curr) => {
+          const quantifier = curr.quantifier;
+          if (!acc.has(quantifier)) {
+            acc.set(quantifier, []);
+          }
+          acc.get(quantifier)?.push(curr);
+          return acc;
+        },
+        new Map<string, QuantificationDto[]>()
+      );
+    },
+});
+
+export interface QuantifierQuantStats {
+  _id: string;
+  totalQuantifications: number;
+  minScore: number;
+  maxScore: number;
+  meanScore: number;
+  medianScore: number;
+  totalScore: number;
+}
+
+const median = (values: number[]): number => {
+  const sorted = values.sort((a, b) => a - b);
+  const half = Math.floor(values.length / 2);
+  if (values.length % 2) {
+    return sorted[half];
+  } else {
+    return (sorted[half - 1] + sorted[half]) / 2.0;
+  }
+};
+export const PeriodQuantifierStats = selectorFamily({
+  key: 'PeriodQuantifierStats',
+  get:
+    (periodId: string | undefined) =>
+    ({ get }): QuantifierQuantStats[] | undefined => {
+      if (!periodId) return undefined;
+      const quantifierQuants = get(
+        AllPeriodQuantificationsGroupedByQuantifier(periodId)
+      );
+      if (!quantifierQuants) return undefined;
+      const quantifierQuantStats: QuantifierQuantStats[] = [];
+      quantifierQuants.forEach((quantifications) => {
+        const quantifier = quantifications[0].quantifier;
+        const scores = quantifications.map((q) => q.score);
+        const totalScore = scores.reduce((acc, curr) => acc + curr, 0);
+        let minScore = Math.min(...scores.filter(Number));
+        isFinite(minScore) || (minScore = 0);
+        const maxScore = Math.max(...scores);
+        const meanScore =
+          quantifications.reduce((acc, curr) => acc + curr.score, 0) /
+          quantifications.length;
+        const medianScore = median(scores);
+        quantifierQuantStats.push({
+          _id: quantifier,
+          totalQuantifications: quantifications.length,
+          minScore,
+          maxScore,
+          meanScore,
+          medianScore,
+          totalScore,
+        });
+      });
+      return quantifierQuantStats;
     },
 });
 
