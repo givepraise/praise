4683834b46c551979ea29e1aac6037e2c5610084
kristoferlund <kristofer@fmckl.se>
2022-08-31 18:11:40 +0200

    Placeholders for errors and loading.

--- packages/frontend/src/components/ErrorPlaceholder.tsx
+++ packages/frontend/src/components/ErrorPlaceholder.tsx
@@ -3,16 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface ErrorPlaceholderProps {
   height: number;
+  error?: Error;
 }
 export const ErrorPlaceholder = ({
   height,
+  error,
 }: ErrorPlaceholderProps): JSX.Element => {
   return (
     <div
-      className="flex items-center justify-center w-full bg-warm-gray-100 rounded-xl"
+      className="flex items-center justify-center w-full h-full bg-warm-gray-100 rounded-xl dark:bg-slate-500"
       style={{ height }}
     >
       <FontAwesomeIcon icon={faTriangleExclamation} size="2x" />
+      {error && <div>{error.message}</div>}
     </div>
   );
 };
--- packages/frontend/src/components/LoadPlaceholder.tsx
+++ packages/frontend/src/components/LoadPlaceholder.tsx
@@ -1,4 +1,4 @@
-import { LoaderSpinner } from '../ui/LoaderSpinner';
+import { LoaderSpinner } from './ui/LoaderSpinner';
 
 interface LoadPlaceholderProps {
   height: number;
@@ -8,7 +8,7 @@ export const LoadPlaceholder = ({
 }: LoadPlaceholderProps): JSX.Element => {
   return (
     <div
-      className="flex items-center w-full bg-warm-gray-100 rounded-xl"
+      className="flex items-center w-full bg-warm-gray-100 rounded-xl dark:bg-slate-500"
       style={{ height }}
     >
       <LoaderSpinner />
