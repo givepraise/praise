5e6f3cf64b6945f7fb5fe6dd010b62efa233a3eb
kristoferlund <kristofer@fmckl.se>
2022-08-31 18:13:28 +0200

    Show only "allowed values" in score distribution

--- packages/frontend/src/pages/Periods/Details/components/analytics/ScoreDistribution.tsx
+++ packages/frontend/src/pages/Periods/Details/components/analytics/ScoreDistribution.tsx
@@ -2,25 +2,50 @@ import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { ChartData, ChartOptions } from 'chart.js';
 import { PeriodPageParams } from '@/model/periods';
-import { ErrorPlaceholder } from '@/components/analytics/ErrorPlaceholder';
 import { PeriodQuantScoreDistribution } from '@/model/periodAnalytics';
+import { SinglePeriodSettingValueRealized } from '@/model/periodsettings';
 import { VerticalBar } from './VerticalBar';
 
-export const ScoreDistribution = (): JSX.Element => {
+export const ScoreDistribution = (): JSX.Element | null => {
   const { periodId } = useParams<PeriodPageParams>();
   const scoreDistribution = useRecoilValue(
     PeriodQuantScoreDistribution(periodId)
   );
+  const allowedValues = useRecoilValue(
+    SinglePeriodSettingValueRealized({
+      periodId,
+      key: 'PRAISE_QUANTIFY_ALLOWED_VALUES',
+    })
+  ) as number[];
 
-  if (!scoreDistribution) {
-    return <ErrorPlaceholder height={375} />;
-  }
+  if (!scoreDistribution) return null;
 
   const sortedScoreDistribution = [...scoreDistribution.entries()].sort(
     (a, b) => a[0] - b[0]
   );
+  const scoreDistributionOnlyAllowedValues = sortedScoreDistribution.filter(
+    (entry) => allowedValues.includes(entry[0])
+  );
 
   const options: ChartOptions<'bar'> = {
+    scales: {
+      x: {
+        ticks: {
+          color: 'rgb(168, 162, 158)',
+        },
+        grid: {
+          color: 'rgb(168, 162, 158, 0.5)',
+        },
+      },
+      y: {
+        ticks: {
+          color: 'rgb(168, 162, 158)',
+        },
+        grid: {
+          color: 'rgb(168, 162, 158, 0.5)',
+        },
+      },
+    },
     plugins: {
       legend: {
         display: false,
@@ -37,12 +62,12 @@ export const ScoreDistribution = (): JSX.Element => {
 
   const data: ChartData<'bar', number[], unknown> = {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    labels: sortedScoreDistribution.map(([score, count]) => score),
+    labels: scoreDistributionOnlyAllowedValues.map(([score, count]) => score),
     datasets: [
       {
         label: 'Count',
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        data: sortedScoreDistribution.map(([score, count]) => count),
+        data: scoreDistributionOnlyAllowedValues.map(([score, count]) => count),
         backgroundColor: 'rgb(225, 0, 127, 0.8)',
       },
     ],
