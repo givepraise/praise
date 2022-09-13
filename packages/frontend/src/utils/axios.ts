cf64abaf402f99c193297912a6aebef3bd7f1bb5
kristoferlund <kristofer@fmckl.se>
2022-09-13 16:05:13 +0200

    Rework error handling to display better error messages

--- packages/frontend/src/utils/axios.ts
+++ packages/frontend/src/utils/axios.ts
@@ -1,7 +1,5 @@
 import axios, { AxiosError, AxiosInstance } from 'axios';
 import { toast } from 'react-hot-toast';
-import { isAppError } from 'api/dist/error/utils';
-import { AppError } from 'api/dist/error/types';
 
 /**
  * Handle error responses (excluding initial 401 response). Any HTTP Code which is not 2xx will be considered as error
@@ -10,34 +8,36 @@ import { AppError } from 'api/dist/error/types';
  */
 export const handleErrors = (err: AxiosError): AxiosError => {
   // Any HTTP Code which is not 2xx will be considered as error
-  const statusCode = err?.response?.status;
 
-  if (err?.request && !err?.response) {
-    toast.error('Server did not respond');
-  } else if (err.response.status === 404) {
-    // Resource not found
-    // Redirect to 404 page
-    window.location.href = '/404';
-  } else if ([403, 400].includes(err.response.status)) {
-    // Forbidden or bad request
-    const isJsonBlob = (data): data is Blob =>
-      data instanceof Blob && data.type === 'application/json';
-    // If the response is a json blob, parse it and display the error message
-    if (isJsonBlob(err.response.data)) {
-      void (err.response.data as Blob).text().then((text) => {
-        const json = JSON.parse(text);
-        toast.error(json.message);
-      });
-    } else if (isAppError(err.response.data)) {
-      toast.error((err.response.data as AppError).message);
+  if (err?.response) {
+    const statusCode = err?.response?.status;
+    if (statusCode === 404) {
+      // Resource not found
+      // Redirect to 404 page
+      window.location.href = '/404';
+    } else if ([403, 400].includes(statusCode)) {
+      // Forbidden or bad request
+      const isJsonBlob = (data): data is Blob =>
+        data instanceof Blob && data.type === 'application/json';
+      // If the response is a json blob, parse it and display the error message
+      if (isJsonBlob(err.response.data)) {
+        void (err.response.data as Blob).text().then((text) => {
+          const json = JSON.parse(text);
+          toast.error(json.message);
+        });
+      } else if ((err.response.data as Error).message) {
+        toast.error((err.response.data as Error).message);
+      } else {
+        toast.error('Something went wrong');
+      }
+    } else if (statusCode === 401) {
+      // Unauthorized
+      window.location.href = '/';
     } else {
-      toast.error('Something went wrong');
+      toast.error('Unknown Error');
     }
-  } else if (err.response.status === 401) {
-    // Unauthorized
-    window.location.href = '/';
-  } else {
-    toast.error('Unknown Error');
+  } else if (!err.response) {
+    toast.error('Server did not respond');
   }
   return err;
 };
