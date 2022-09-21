8f74cefb1e5e0d0f2891cf87ee2428f5084adaf4
kristoferlund <kristofer@fmckl.se>
2022-09-21 16:45:28 +0200

    JSON formatting for readability

--- packages/api/src/settings/transformers.ts
+++ packages/api/src/settings/transformers.ts
@@ -7,18 +7,12 @@ import { SettingDocument, SettingDto } from './types';
  * @returns {SettingDto}
  */
 export const settingTransformer = (setting: SettingDocument): SettingDto => {
-  const {
-    _id,
-    key,
-    value,
-    valueRealized,
-    type,
-    label,
-    description,
-    group,
-    options,
-  } = setting;
-
+  const { _id, key, type, label, description, group, options } = setting;
+  let { value, valueRealized } = setting;
+  if (type === 'JSON') {
+    value = JSON.stringify(JSON.parse(value), null, 2);
+    valueRealized = JSON.stringify(JSON.parse(value), null, 2);
+  }
   return {
     _id,
     key,
