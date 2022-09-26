45f81c14092561f67620d46c1f8d88e05900bee9
kristoferlund <kristofer@fmckl.se>
2022-09-26 11:26:57 +0200

    v0.12.0

--- packages/frontend/src/model/app.ts
+++ packages/frontend/src/model/app.ts
@@ -33,7 +33,7 @@ interface PraiseAppVersion {
 
 export const usePraiseAppVersion = (): PraiseAppVersion => {
   const appVersion: PraiseAppVersion = {
-    current: '0.11.2', //TODO: get this from package.json
+    current: '0.12.0', //TODO: get this from package.json
     latest: undefined,
     newVersionAvailable: false,
   };
