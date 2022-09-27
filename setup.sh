f6732f7d043d183bc0ff46b289e6c2c2fbb8c2c0
Nebs <nebojsa.stojanovic0@gmail.com>
2022-09-27 12:37:50 +0200

    modified setup script to use prebuilt image

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-IMAGE=$( docker images | grep praise-setup )
+IMAGE=$( docker images | grep ghcr.io/commons-stack/praise/setup:latest )
 if [ -z "$IMAGE" ]
 then
-  echo "Building setup image..."
-  docker build -t praise-setup -q -f ./packages/setup/Dockerfile .
+  echo "Pulling the setup image..."
+  docker image pull ghcr.io/commons-stack/praise/setup:latest
 fi
 
-docker run -it -v $(pwd):/usr/praise praise-setup
+docker run -it -v $(pwd):/usr/praise ghcr.io/commons-stack/praise/setup:latest
