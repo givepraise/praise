9129a5b3dfb8c2cce7fd51f62ffe5f974b1a2e62
kristoferlund <kristofer@fmckl.se>
2022-10-03 16:18:13 +0200

    Rewrote mongorestore connection string etc

--- database-restore.sh
+++ database-restore.sh
@@ -7,5 +7,5 @@ export $(grep -v '^#' .env | xargs)
 docker compose -f ./docker-compose.production.yml down
 docker volume rm $(docker volume ls -q)
 docker compose -f ./docker-compose.production.yml up mongodb -d --remove-orphans
-docker exec -i mongodb-praise sh -c 'mongorestore --authenticationDatabase admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --nsInclude=praise_db.* --archive' < $1
+docker exec -i mongodb-praise sh -c 'mongorestore --authenticationDatabase admin --nsInclude=praise_db.* --uri="mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@mongodb:27017/?authSource=admin" --drop --preserveUUID --archive' < $1
 docker compose -f ./docker-compose.production.yml up -d --remove-orphans
