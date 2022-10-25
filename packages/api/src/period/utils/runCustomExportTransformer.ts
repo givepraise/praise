d5b06a12905f7072436fd6ee9f92109f8d207eb1
kristoferlund <kristofer@fmckl.se>
2022-10-25 16:29:58 +0200

    Switch to use ses-node-json-transformer

---
+++ packages/api/src/period/utils/runCustomExportTransformer.ts
@@ -0,0 +1,12 @@
+import { transform } from 'ses-node-json-transform';
+import { ExportTransformer, PeriodDetailsGiverReceiverDto } from '../types';
+
+export const runCustomExportTransformer = (
+  data: PeriodDetailsGiverReceiverDto[],
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+  context: any,
+  transformer: ExportTransformer
+): Object[] => {
+  const result = transform(data, transformer.map, context);
+  return result;
+};
