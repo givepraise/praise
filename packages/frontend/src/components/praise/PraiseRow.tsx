1eaa1c7a6e87f9fab35e6ed265728608ad0786a5
Nebs <nebojsa.stojanovic0@gmail.com>
2022-11-04 08:46:00 +0100

    remove unnecessary code from a PraiseRow

--- packages/frontend/src/components/praise/PraiseRow.tsx
+++ packages/frontend/src/components/praise/PraiseRow.tsx
@@ -1,28 +1,11 @@
 import { PraiseDto } from 'api/dist/praise/types';
-import { useHistory } from 'react-router-dom';
 
 interface PraiseRowProps {
   praise: PraiseDto;
   children: JSX.Element;
 }
 
-export const PraiseRow = ({
-  praise,
-  children,
-}: PraiseRowProps): JSX.Element => {
-  const history = useHistory();
-
-  const handleClick =
-    (data: PraiseDto) => (event: React.MouseEvent<HTMLTableRowElement>) => {
-      const element = event.target as HTMLElement;
-
-      console.log('ELEMENT:', element.tagName);
-
-      // if (element.tagName !== 'A') {
-      //   history.push(`/praise/${data._id}`);
-      // }
-    };
-
+export const PraiseRow = ({ children }: PraiseRowProps): JSX.Element => {
   return (
     <li className="md:p-5 first:rounded-t-lg hover:bg-warm-gray-100 dark:hover:bg-slate-500">
       <div>{children}</div>
