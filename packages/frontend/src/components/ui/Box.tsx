83784114a209a1fb8a1414d693832b502a445ba0
kristoferlund <kristofer@fmckl.se>
2022-11-18 10:39:12 +0100

    Add support for forwardRef

--- packages/frontend/src/components/ui/Box.tsx
+++ packages/frontend/src/components/ui/Box.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { classNames } from '../../utils/';
 
 type BoxVariant = 'basic' | 'wide';
@@ -14,33 +15,34 @@ interface BoxProps {
     | JSX.Element[];
 }
 
-export const Box = ({
-  variant,
-  className,
-  ref,
-  children,
-}: BoxProps): JSX.Element => {
-  const baseClass =
-    'border shadow-none md:shadow-md rounded-none md:rounded-xl bg-warm-gray-50 dark:bg-slate-600';
-  const defaultClass = `w-full md:w-[710px] p-5 ${baseClass}`;
-  const wideClass = `w-full md:w-[710px] xl:w-[960px] p-5 ${baseClass}`;
+// eslint-disable-next-line react/display-name
+export const Box = React.forwardRef(
+  (
+    { variant, className, children }: BoxProps,
+    ref: React.LegacyRef<HTMLDivElement> | undefined
+  ): JSX.Element => {
+    const baseClass =
+      'border shadow-none md:shadow-md rounded-none md:rounded-xl bg-warm-gray-50 dark:bg-slate-600';
+    const defaultClass = `w-full md:w-[710px] p-5 ${baseClass}`;
+    const wideClass = `w-full md:w-[710px] xl:w-[960px] p-5 ${baseClass}`;
 
-  let variantClass = defaultClass;
-  switch (variant) {
-    case 'basic':
-      variantClass = baseClass;
-      break;
-    case 'wide':
-      variantClass = wideClass;
-      break;
-    default:
-      variantClass = defaultClass;
-      break;
+    let variantClass = defaultClass;
+    switch (variant) {
+      case 'basic':
+        variantClass = baseClass;
+        break;
+      case 'wide':
+        variantClass = wideClass;
+        break;
+      default:
+        variantClass = defaultClass;
+        break;
+    }
+
+    return (
+      <div className={classNames(variantClass, className)} ref={ref}>
+        {children}
+      </div>
+    );
   }
-
-  return (
-    <div className={classNames(variantClass, className)} ref={ref}>
-      {children}
-    </div>
-  );
-};
+);
