646e0f9794d0eb5800cd515fc528902d9da1c9f4
kkatusic <kkatusic@gmail.com>
2022-12-07 20:01:07 +0100

    Users test files started

---
+++ packages/api2/src/users/test/users.controller.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../schemas/users.schema';
+import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
+// import { userStub } from './stubs/user.stub';
+
+describe('AuthController', () => {
+  let usersController: UsersController;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      controllers: [UsersController],
+      providers: [UsersService],
+    }).compile();
+
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    jest.clearAllMocks();
+  });
+
+  describe('findAll', () => {
+    let users: User[];
+    beforeEach(async () => {
+      users = await usersController.findAll();
+      jest.clearAllMocks();
+    });
+
+    test('findAll should call usersService', async () => {
+      expect(usersService.findAll).toHaveBeenCalled();
+    });
+  });
+});
