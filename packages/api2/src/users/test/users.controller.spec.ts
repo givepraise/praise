208b150618ad72dfc22f2817c98b8626b1f35884
kkatusic <kkatusic@gmail.com>
2022-12-12 09:20:25 +0100

    Problem with findOne

--- packages/api2/src/users/test/users.controller.spec.ts
+++ packages/api2/src/users/test/users.controller.spec.ts
@@ -2,7 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { User } from '../schemas/users.schema';
 import { UsersController } from '../users.controller';
 import { UsersService } from '../users.service';
+import { Request } from 'express';
 import { userStub } from './stubs/user.stub';
+import { REQUEST } from '@nestjs/core/router/request/request-constants';
+// import { REQUEST } from '@nestjs/core/router';
+// import { REQUEST } from '@nestjs/core';
+// import { REQUEST } from '@nestjs/core/router/request';
 
 jest.mock('@/users/users.service');
 
@@ -14,7 +19,13 @@ describe('UsersController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: REQUEST,
+          useValue: Request,
+        },
+      ],
     }).compile();
 
     usersController = module.get<UsersController>(UsersController);
@@ -29,14 +40,31 @@ describe('UsersController', () => {
       jest.clearAllMocks();
     });
 
-    test('findAll should call usersService', async () => {
+    test('should call usersService', async () => {
       users = await usersController.findAll();
       expect(usersService.findAll).toHaveBeenCalled();
     });
 
-    test('findAll should return an array of users', async () => {
+    test('should return an array of users', async () => {
       users = await usersController.findAll();
       expect(users).toEqual([userStub]);
     });
+  });
+
+  describe('findOne', () => {
+    let user: User;
+    beforeEach(async () => {
+      jest.clearAllMocks();
+    });
+
+    test('should call usersService', async () => {
+      user = await usersController.findOne(Request, userStub._id);
+      expect(usersService.findOneById).toBeCalledWith(userStub._id);
+    });
+
+    // test('should return an array of one user', async () => {
+    //   users = await usersController.findAll();
+    //   expect(users).toEqual([userStub]);
+    // });
   });
 });
