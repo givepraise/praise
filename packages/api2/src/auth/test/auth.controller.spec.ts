0941d1671224a330e0df27b2f2114d43d9e728ef
kristoferlund <kristofer@fmckl.se>
2022-12-14 19:12:11 +0100

    Added missing mock EventLogService

--- packages/api2/src/auth/test/auth.controller.spec.ts
+++ packages/api2/src/auth/test/auth.controller.spec.ts
@@ -3,9 +3,11 @@ import { AuthController } from '../auth.controller';
 import { EthSignatureService } from '../eth-signature.service';
 import { UsersService } from '@/users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { EventLogService } from '@/event-log/event-log.service';
 
 jest.mock('@/users/users.service');
 jest.mock('@/auth/eth-signature.service');
+jest.mock('@/event-log/event-log.service');
 
 describe('AuthController', () => {
   let authController: AuthController;
@@ -15,7 +17,12 @@ describe('AuthController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [],
       controllers: [AuthController],
-      providers: [EthSignatureService, UsersService, JwtService],
+      providers: [
+        EthSignatureService,
+        UsersService,
+        JwtService,
+        EventLogService,
+      ],
     }).compile();
 
     authController = module.get<AuthController>(AuthController);
