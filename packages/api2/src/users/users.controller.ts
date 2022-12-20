2ed26e3d35465f4807d5859abf099bffa8f4e096
kristoferlund <kristofer@fmckl.se>
2022-12-20 10:36:42 +0100

    Add `api-key` authentication for all users and event-log endpoints

--- packages/api2/src/users/users.controller.ts
+++ packages/api2/src/users/users.controller.ts
@@ -17,13 +17,12 @@ import { Types } from 'mongoose';
 import { ObjectIdPipe } from '../shared/pipes/object-id.pipe';
 import { User } from './schemas/users.schema';
 import { ApiParam } from '@nestjs/swagger';
-import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { Permissions } from '@/auth/decorators/permissions.decorator';
 import { Permission } from '@/auth/enums/permission.enum';
 import { PermissionsGuard } from '@/auth/guards/permissions.guard';
 import { EventLogService } from '@/event-log/event-log.service';
-import { EventLogTypeKey } from '@/event-log/enums/event-log-type-key';
 import { RequestWithUser } from '@/auth/interfaces/request-with-user.interface';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 @SerializeOptions({
@@ -31,7 +30,7 @@ import { RequestWithUser } from '@/auth/interfaces/request-with-user.interface';
 })
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(PermissionsGuard)
-@UseGuards(JwtAuthGuard)
+@UseGuards(AuthGuard(['jwt', 'api-key']))
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
