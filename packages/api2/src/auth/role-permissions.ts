68b4f91e3625265d16e82e70a330ebe23d5a6df9
kristoferlund <kristofer@fmckl.se>
2022-12-20 10:35:27 +0100

    Set initial permissions for access with API key Refactored users permissions etc

--- packages/api2/src/auth/role-permissions.ts
+++ packages/api2/src/auth/role-permissions.ts
@@ -5,13 +5,26 @@ import { Permission } from './enums/permission.enum';
  *  PermissionsGuard to check if the user has the required permissions to access
  *  the route.
  */
+const PERMISSIONS_USER = [
+  Permission.UsersFind,
+  Permission.EventLogView,
+  Permission.ApiKeyView,
+];
+const PERMISSIONS_FORWARDER = [...PERMISSIONS_USER];
+const PERMISSIONS_QUANTIFIER = [...PERMISSIONS_USER];
+const PERMISSIONS_ADMIN = [
+  ...PERMISSIONS_USER,
+  Permission.ApiKeyManage,
+  Permission.UsersManageRoles,
+];
+const PERMISSION_APIKEY_READ = [...PERMISSIONS_USER];
+const PERMISSION_APIKEY_READWRITE = [...PERMISSIONS_USER];
+
 export const RolePermissions: { [key: string]: string[] } = {
-  ADMIN: [
-    Permission.ApiKeyView,
-    Permission.ApiKeyManage,
-    Permission.UsersFind,
-    Permission.UsersManageRoles,
-    Permission.EventLogView,
-  ],
-  USER: [Permission.UsersFind, Permission.EventLogView, Permission.ApiKeyView],
+  USER: PERMISSIONS_USER,
+  FORWARDER: PERMISSIONS_FORWARDER,
+  QUANTIFIER: PERMISSIONS_QUANTIFIER,
+  ADMIN: PERMISSIONS_ADMIN,
+  APIKEY_READ: PERMISSION_APIKEY_READ,
+  APIKEY_READWRITE: PERMISSION_APIKEY_READWRITE,
 };
