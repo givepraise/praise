583e50096e010de29ee7d952ac4691e5c2ce878b
kristoferlund <kristofer@fmckl.se>
2023-01-05 12:50:36 +0100

    Frontend local settings DTOs

---
+++ packages/frontend/src/model/settings/dto/setting.dto.ts
@@ -0,0 +1,15 @@
+import { SettingGroup } from '../enums/setting-group.enum';
+
+export interface SettingDto {
+  _id: string;
+  key: string;
+  label: string;
+  description?: string;
+  defaultValue?: string;
+  value?: string;
+  valueRealized?: string | boolean | number | number[] | string[] | object;
+  type: string;
+  options?: string;
+  group: SettingGroup;
+  subgroup: number;
+}
---
+++ packages/frontend/src/model/settings/enums/setting-group.enum.ts
@@ -0,0 +1,6 @@
+export declare enum SettingGroup {
+  APPLICATION = 0,
+  PERIOD_DEFAULT = 1,
+  DISCORD = 2,
+  CUSTOM_EXPORT = 3,
+}
