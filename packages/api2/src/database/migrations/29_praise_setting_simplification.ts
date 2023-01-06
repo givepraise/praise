f31c72b44e6ccfa88533424cf7c30515a6490305
kristoferlund <kristofer@fmckl.se>
2023-01-06 11:44:19 +0100

    Drop index before migration

--- packages/api2/src/database/migrations/29_praise_setting_simplification.ts
+++ packages/api2/src/database/migrations/29_praise_setting_simplification.ts
@@ -1,9 +1,16 @@
-import mongoose from 'mongoose';
 import { MigrationsContext } from '../interfaces/migration-context.interface';
 import { PeriodSettingsModel } from '../schemas/periodsettings/23_periodsettings.schema';
-import { PeriodSettingsSchema } from '../schemas/periodsettings/29_periodsettings.schema';
 
 const up = async ({ context }: MigrationsContext): Promise<void> => {
+  // Check if the index exists
+  const indexExists = await PeriodSettingsModel.collection.indexExists(
+    'key_1_period_1',
+  );
+
+  if (indexExists) {
+    await PeriodSettingsModel.collection.dropIndex('key_1_period_1');
+  }
+
   // invoke previous model with all keys to delete them
   await PeriodSettingsModel.updateMany(
     {},
@@ -16,14 +23,10 @@ const up = async ({ context }: MigrationsContext): Promise<void> => {
         group: 1,
         options: 1,
         subgroup: 1,
-        periodOverridable: 1
+        periodOverridable: 1,
       },
-    }
+    },
   );
-
-  // Delete previous model and add new one
-  delete mongoose.models['PeriodSettings'];
-  mongoose.model('PeriodSettings', PeriodSettingsSchema);
 };
 
 export { up };
