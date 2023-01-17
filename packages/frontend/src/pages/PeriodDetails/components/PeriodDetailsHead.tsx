410922394fe428d1b7d12fc8dc257db0cefebd85
kristoferlund <kristofer@fmckl.se>
2023-01-17 22:54:16 +0100

    Set free the assign quantifiers button

--- packages/frontend/src/pages/PeriodDetails/components/PeriodDetailsHead.tsx
+++ packages/frontend/src/pages/PeriodDetails/components/PeriodDetailsHead.tsx
@@ -6,28 +6,19 @@ import {
   PeriodPageParams,
   useLoadSinglePeriodDetails,
   SinglePeriod,
-  useAssignQuantifiers,
 } from '@/model/periods/periods';
 import { AllQuantifierUsers } from '@/model/user/users';
 import { formatIsoDateUTC, DATE_FORMAT } from '@/utils/date';
 import { getPreviousPeriod } from '@/utils/periods';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Dialog } from '@headlessui/react';
-import { Button } from '@mui/material';
-import React from 'react';
-import toast from 'react-hot-toast';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { PeriodAssignDialog } from './AssignDialog';
 import { CloseButton } from './CloseButton';
 import { ExportDropdown } from './ExportDropdown';
 import { PeriodDateForm } from './PeriodDateForm';
 import { PeriodNameForm } from './PeriodNameForm';
+import { AssignButton } from './AssignButton';
 
 export const PeriodDetailsHead = (): JSX.Element | null => {
-  const [isAssignDialogOpen, setIsAssignDialogOpen] = React.useState(false);
-
   const allPeriods = useRecoilValue(AllPeriods);
   const allQuantifiers = useRecoilValue(AllQuantifierUsers);
   const { periodId } = useParams<PeriodPageParams>();
@@ -36,41 +27,9 @@ export const PeriodDetailsHead = (): JSX.Element | null => {
   const isAdmin = useRecoilValue(HasRole(ROLE_ADMIN));
   const periodStats = useRecoilValue(PeriodStatsSelector(periodId));
 
-  const history = useHistory();
-
-  const assignDialogRef = React.useRef(null);
-
-  const { assignQuantifiers } = useAssignQuantifiers(periodId);
-
   if (!period || !allPeriods) return null;
 
   const previousPeriod = getPreviousPeriod(allPeriods, period);
-
-  const handleAssign = (): void => {
-    const toastId = 'assignToast';
-    const promise = assignQuantifiers();
-    void toast.promise(
-      promise,
-      {
-        loading: 'Assigning quantifiers …',
-        success: () => {
-          setTimeout(() => history.go(0), 2000);
-          return 'Quantifiers assigned';
-        },
-        error: () => {
-          setTimeout(() => toast.remove(toastId), 2000);
-          return 'Assign failed';
-        },
-      },
-      {
-        id: toastId,
-        position: 'top-center',
-        loading: {
-          duration: Infinity,
-        },
-      }
-    );
-  };
 
   if (!period) return <div>Period not found.</div>;
 
@@ -114,18 +73,7 @@ export const PeriodDetailsHead = (): JSX.Element | null => {
                 period?.receivers.length > 0 &&
                 allQuantifiers &&
                 allQuantifiers.length > 0 ? (
-                  <Button
-                    onClick={(): void => {
-                      setIsAssignDialogOpen(true);
-                    }}
-                  >
-                    <FontAwesomeIcon
-                      icon={faUsers}
-                      size="1x"
-                      className="mr-2"
-                    />
-                    Assign quantifiers
-                  </Button>
+                  <AssignButton />
                 ) : null}
                 {period.status === 'QUANTIFY' && isAdmin ? (
                   <ExportDropdown />
@@ -138,22 +86,6 @@ export const PeriodDetailsHead = (): JSX.Element | null => {
           </div>
         </>
       )}
-
-      {period.status === 'OPEN' && isAdmin ? (
-        <Dialog
-          open={isAssignDialogOpen}
-          onClose={(): void => setIsAssignDialogOpen(false)}
-          className="fixed inset-0 z-10 overflow-y-auto"
-          initialFocus={assignDialogRef}
-        >
-          <div ref={assignDialogRef}>
-            <PeriodAssignDialog
-              onClose={(): void => setIsAssignDialogOpen(false)}
-              onAssign={(): void => handleAssign()}
-            />
-          </div>
-        </Dialog>
-      ) : null}
     </div>
   );
 };
