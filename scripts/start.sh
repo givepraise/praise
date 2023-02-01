f6f1bc5bf0ac87086216044b9a17a6bfcc7fe591
kristoferlund <kristofer@fmckl.se>
2023-02-01 10:21:59 +0100

    Revert setup script changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f ../docker-compose.production.yml up -d
+docker compose -f ../docker-compose.production.yml up -d
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bash ./database-backup.sh
-docker-compose -f ../docker-compose.production.yml pull
-docker-compose -f ../docker-compose.production.yml down
-docker-compose -f ../docker-compose.production.yml up -d --remove-orphans
+docker compose -f ../docker-compose.production.yml pull
+docker compose -f ../docker-compose.production.yml down
+docker compose -f ../docker-compose.production.yml up -d --remove-orphans
