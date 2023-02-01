db20ec9110c8fdd51beff32b45820738c14df970
kristoferlund <kristofer@fmckl.se>
2023-02-01 12:18:19 +0100

    Move scripts back to root folder

--- scripts/upgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-bash ./database-backup.sh
-docker compose -f ../docker-compose.production.yml pull
-docker compose -f ../docker-compose.production.yml down
-docker compose -f ../docker-compose.production.yml up -d --remove-orphans
