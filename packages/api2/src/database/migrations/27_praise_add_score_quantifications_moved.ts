66bb80b6a22cb3916d1369934bf3b9a70c48e082
Nebs <nebojsa.stojanovic0@gmail.com>
2023-02-09 18:33:07 +0100

    fix issue with a quantification migration - create new period settings for each default setting

--- packages/api2/src/database/migrations/27_praise_add_score_quantifications_moved.ts
+++ packages/api2/src/database/migrations/27_praise_add_score_quantifications_moved.ts
@@ -1,7 +1,40 @@
 import { MigrationsContext } from '../interfaces/migration-context.interface';
+import { PeriodModel } from '../schemas/period/period.schema';
+import { PeriodSettingsModel } from '../schemas/periodsettings/23_periodsettings.schema';
 import { PraiseModel } from '../schemas/praise/27_praise_schema';
 
 const up = async ({ context }: MigrationsContext): Promise<void> => {
+  enum SettingGroup {
+    APPLICATION,
+    PERIOD_DEFAULT,
+    DISCORD,
+    CUSTOM_EXPORT,
+  }
+
+  const allPeriods = await PeriodModel.find();
+
+  await PeriodSettingsModel.deleteMany({ group: SettingGroup.PERIOD_DEFAULT });
+  for await (const period of allPeriods) {
+    const periodSettingsDefaults = await context.settingsService.findByGroup(
+      SettingGroup.PERIOD_DEFAULT,
+    );
+
+    const periodSettings = periodSettingsDefaults.map((setting) => {
+      return {
+        key: setting.key,
+        period: period._id,
+        setting: setting._id,
+        value: setting.value,
+        group: setting.group,
+        periodOverridable: setting.periodOverridable,
+        label: setting.label,
+        type: setting.type,
+      };
+    });
+
+    PeriodSettingsModel.insertMany(periodSettings);
+  }
+
   const praises = await PraiseModel.find({
     score: { $exists: false },
   });
