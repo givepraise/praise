f963b5bb2464bb56ada7a15e09bc72372d506a86
kristoferlund <kristofer@fmckl.se>
2023-02-10 10:39:32 +0100

    Add --latest support to database-restore.sh

--- database-restore.sh
+++ database-restore.sh
@@ -3,8 +3,17 @@ if ! test  -f "$1" ; then
   echo "Could not find database backup"
   echo "Usage: database-restore.sh filename\n"
 fi
+
+if  [[ $2 = "--latest" ]]; then
+  echo "Using latest version"
+  VERSION="latest"
+else
+  echo "Using release version"
+  VERSION="release"
+fi
+
 export $(grep -v '^#' ./.env | xargs)
-docker compose -f ./docker-compose.production.yml down -v
-docker compose -f ./docker-compose.production.yml up mongodb -d --remove-orphans
+docker compose -f ./docker-compose.$VERSION.yml down -v
+docker compose -f ./docker-compose.$VERSION.yml up mongodb -d --remove-orphans
 docker exec -i mongodb-praise sh -c 'mongorestore --authenticationDatabase admin --nsInclude=praise_db.* --uri="mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@mongodb:$MONGO_PORT/?authSource=admin" --drop --preserveUUID --archive' < $1
-docker compose -f ./docker-compose.production.yml up -d --remove-orphans
+docker compose -f ./docker-compose.$VERSION.yml up -d --remove-orphans
