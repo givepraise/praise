1f3cd3046c13fbea6d186303a2b1e755c9464d46
kristoferlund <kristofer@fmckl.se>
2023-02-16 14:29:43 +0100

    Use provided `valueRealized` property

--- packages/api2/src/auth/strategies/bypass.strategy.ts
+++ packages/api2/src/auth/strategies/bypass.strategy.ts
@@ -13,17 +13,17 @@ export class BypassStrategy extends PassportStrategy(Strategy, 'bypass') {
     super();
   }
   async validate(req: Request): Promise<any> {
-    const bypassRoutesSettingValue = (await this.settingsService.settingValue(
+    const bypassRoutesSetting = await this.settingsService.findOneByKey(
       'BYPASS_ROUTE_AUTH',
-    )) as string;
+    );
 
     // No bypass routes configured means no bypass
-    if (!bypassRoutesSettingValue) {
+    if (!bypassRoutesSetting) {
       return null;
     }
 
     // Check if the current route is in the bypass list
-    const bypassRoutesList = bypassRoutesSettingValue.split(',');
+    const bypassRoutesList = bypassRoutesSetting.valueRealized as string[];
     if (bypassRoutesList.includes(req.url)) {
       // If so, return a dummy user with the USER role
       return { roles: ['USER'] };
