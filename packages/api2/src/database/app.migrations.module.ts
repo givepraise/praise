ca682b5a548ed130a9a24b042b85a82a42fce3f7
kristoferlund <kristofer@fmckl.se>
2023-03-14 14:56:31 +0100

    Separate app for migrations

---
+++ packages/api2/src/database/app.migrations.module.ts
@@ -0,0 +1,37 @@
+import { AuthModule } from '../auth/auth.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserAccountsModule } from '../useraccounts/useraccounts.module';
+import { UsersModule } from '../users/users.module';
+import { EventLogModule } from '../event-log/event-log.module';
+import { SettingsModule } from '../settings/settings.module';
+import { PeriodSettingsModule } from '../periodsettings/periodsettings.module';
+import { PraiseModule } from '../praise/praise.module';
+import { QuantificationsModule } from '../quantifications/quantifications.module';
+import { RequestContextModule } from 'nestjs-request-context';
+import { PeriodsModule } from '../periods/periods.module';
+import { ApiKeyModule } from '../api-key/api-key.module';
+import { ActivateModule } from '../activate/activate.module';
+import { CommunityModule } from '../community/community.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin&appname=Migrations`,
+    ),
+    ActivateModule,
+    ApiKeyModule,
+    AuthModule,
+    CommunityModule,
+    EventLogModule,
+    PeriodsModule,
+    PeriodSettingsModule,
+    PraiseModule,
+    QuantificationsModule,
+    RequestContextModule,
+    SettingsModule,
+    UserAccountsModule,
+    UsersModule,
+  ],
+})
+export class AppMigrationsModule {}
