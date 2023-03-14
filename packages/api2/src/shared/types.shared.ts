a184ec4c0628fa493e170e12996d5f4a0b0201ff
kristoferlund <kristofer@fmckl.se>
2023-03-14 14:18:18 +0100

    Remove unused

--- packages/api2/src/shared/types.shared.ts
+++
@@ -1,9 +0,0 @@
-import { TransformerMap } from 'ses-node-json-transform';
-
-export interface ExportTransformer {
-  name: string;
-  map: TransformerMap;
-  context: Record<string, unknown>;
-  filterColumn: string;
-  includeCsvHeaderRow?: boolean;
-}
