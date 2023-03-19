71137b72c9ffe0ff37f99097c20ed84879ad7fc5
kristoferlund <kristofer@fmckl.se>
2023-03-19 21:04:32 +0000

    Refactor shared testing code, move to shared folder

--- packages/api2/test/shared/nest.ts
+++ packages/api2/test/shared/nest.ts
@@ -1,47 +1,90 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ServiceExceptionFilter } from '@/shared/filters/service-exception.filter';
-import { UsersService } from '@/users/users.service';
-import { UsersModule } from '@/users/users.module';
-import { UsersSeeder } from '@/database/seeder/users.seeder';
-import { EventLogSeeder } from '@/database/seeder/event-log.seeder';
-import { EventLogModule } from '@/event-log/event-log.module';
-import { EventLogService } from '@/event-log/event-log.service';
-import { runDbMigrations } from '@/database/migrations';
-import { MongoServerErrorFilter } from '@/shared/filters/mongo-server-error.filter';
-import { MongoValidationErrorFilter } from '@/shared/filters/mongo-validation-error.filter';
-import { ActivateModule } from '@/activate/activate.module';
-import { ApiKeyModule } from '@/api-key/api-key.module';
-import { AuthModule } from '@/auth/auth.module';
-import { PeriodsModule } from '@/periods/periods.module';
-import { PeriodSettingsModule } from '@/periodsettings/periodsettings.module';
-import { PraiseModule } from '@/praise/praise.module';
-import { QuantificationsModule } from '@/quantifications/quantifications.module';
-import { SettingsModule } from '@/settings/settings.module';
-import { UserAccountsModule } from '@/useraccounts/useraccounts.module';
+import { Server } from 'http';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RequestContextModule } from 'nestjs-request-context';
-import { CommunityModule } from '@/community/community.module';
-import { Server } from 'http';
-import { ActivateService } from '@/activate/activate.service';
-import { UserAccountsService } from '@/useraccounts/useraccounts.service';
-import { UserAccountsSeeder } from '@/database/seeder/useraccounts.seeder';
-import { ApiKeyService } from '@/api-key/api-key.service';
 
-export let module: TestingModule;
+// Import modules and services
+import { ActivateModule } from '@/activate/activate.module';
+import { ActivateService } from '@/activate/activate.service';
+import { ApiKeyModule } from '@/api-key/api-key.module';
+import { ApiKeyService } from '@/api-key/api-key.service';
+import { AuthModule } from '@/auth/auth.module';
+import { EthSignatureService } from '@/auth/eth-signature.service';
+import { CommunityModule } from '@/community/community.module';
+import { CommunityService } from '@/community/community.service';
+import { EventLogModule } from '@/event-log/event-log.module';
+import { EventLogService } from '@/event-log/event-log.service';
+import { PeriodsModule } from '@/periods/periods.module';
+import { PeriodsService } from '@/periods/services/periods.service';
+import { PeriodAssignmentsService } from '@/periods/services/period-assignments.service';
+import { PeriodSettingsModule } from '@/periodsettings/periodsettings.module';
+import { PeriodSettingsService } from '@/periodsettings/periodsettings.service';
+import { PraiseModule } from '@/praise/praise.module';
+import { PraiseService } from '@/praise/services/praise.service';
+import { QuantificationsModule } from '@/quantifications/quantifications.module';
+import { QuantificationsService } from '@/quantifications/services/quantifications.service';
+import { SettingsModule } from '@/settings/settings.module';
+import { SettingsService } from '@/settings/settings.service';
+import { UserAccountsModule } from '@/useraccounts/useraccounts.module';
+import { UserAccountsService } from '@/useraccounts/useraccounts.service';
+import { UsersModule } from '@/users/users.module';
+import { UsersService } from '@/users/users.service';
+
+// Import filters
+import { ServiceExceptionFilter } from '@/shared/filters/service-exception.filter';
+import { MongoServerErrorFilter } from '@/shared/filters/mongo-server-error.filter';
+import { MongoValidationErrorFilter } from '@/shared/filters/mongo-validation-error.filter';
+
+// Import seeders
+import { ApiKeySeeder } from '@/database/seeder/api-key.seeder';
+import { CommunitiesSeeder } from '@/database/seeder/communities.seeder';
+import { EventLogSeeder } from '@/database/seeder/event-log.seeder';
+import { PeriodsSeeder } from '@/database/seeder/periods.seeder';
+import { PeriodSettingsSeeder } from '@/database/seeder/periodsettings.seeder';
+import { PraiseSeeder } from '@/database/seeder/praise.seeder';
+import { QuantificationsSeeder } from '@/database/seeder/quantifications.seeder';
+import { SettingsSeeder } from '@/database/seeder/settings.seeder';
+import { UsersSeeder } from '@/database/seeder/users.seeder';
+import { UserAccountsSeeder } from '@/database/seeder/useraccounts.seeder';
+
+// Import migrations
+import { runDbMigrations } from '@/database/migrations';
+
+// Core Nest objects
+export let testingModule: TestingModule;
 export let app: INestApplication;
 export let server: Server;
+
+// Services
 export let activateService: ActivateService;
-export let usersSeeder: UsersSeeder;
-export let usersService: UsersService;
-export let userAccountsSeeder: UserAccountsSeeder;
-export let userAccountsService: UserAccountsService;
-export let eventLogSeeder: EventLogSeeder;
-export let eventLogService: EventLogService;
 export let apiKeyService: ApiKeyService;
+export let communityService: CommunityService;
+export let eventLogService: EventLogService;
+export let ethSignatureService: EthSignatureService;
+export let periodsService: PeriodsService;
+export let periodsAssignmentService: PeriodAssignmentsService;
+export let periodSettingsService: PeriodSettingsService;
+export let praiseService: PraiseService;
+export let quantificationsService: QuantificationsService;
+export let settingsService: SettingsService;
+export let userAccountsService: UserAccountsService;
+export let usersService: UsersService;
+
+// Seeders
+export let apiKeySeeder: ApiKeySeeder;
+export let communitiesSeeder: CommunitiesSeeder;
+export let eventLogSeeder: EventLogSeeder;
+export let periodsSeeder: PeriodsSeeder;
+export let periodSettingsSeeder: PeriodSettingsSeeder;
+export let praiseSeeder: PraiseSeeder;
+export let quantificationsSeeder: QuantificationsSeeder;
+export let settingsSeeder: SettingsSeeder;
+export let usersSeeder: UsersSeeder;
+export let userAccountsSeeder: UserAccountsSeeder;
 
 async function startNest(): Promise<void> {
-  module = await Test.createTestingModule({
+  testingModule = await Test.createTestingModule({
     imports: [
       MongooseModule.forRoot(
         `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/test-community?authSource=admin&appname=PraiseApi`,
@@ -60,10 +103,21 @@ async function startNest(): Promise<void> {
       UserAccountsModule,
       UsersModule,
     ],
-    providers: [UsersSeeder, EventLogSeeder, UserAccountsSeeder],
+    providers: [
+      ApiKeySeeder,
+      CommunitiesSeeder,
+      EventLogSeeder,
+      PeriodsSeeder,
+      PeriodSettingsSeeder,
+      PraiseSeeder,
+      QuantificationsSeeder,
+      SettingsSeeder,
+      UserAccountsSeeder,
+      UsersSeeder,
+    ],
   }).compile();
 
-  app = module.createNestApplication();
+  app = testingModule.createNestApplication();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -80,17 +134,50 @@ async function startNest(): Promise<void> {
   // Run DB migrations
   await runDbMigrations(app);
 
-  // Get and return services for convenience
-  usersSeeder = module.get<UsersSeeder>(UsersSeeder);
-  usersService = module.get<UsersService>(UsersService);
-  eventLogSeeder = module.get<EventLogSeeder>(EventLogSeeder);
-  eventLogService = module.get<EventLogService>(EventLogService);
-  activateService = module.get<ActivateService>(ActivateService);
-  userAccountsService = module.get<UserAccountsService>(UserAccountsService);
-  apiKeyService = module.get<ApiKeyService>(ApiKeyService);
-  userAccountsSeeder = module.get<UserAccountsSeeder>(UserAccountsSeeder);
+  // Services
+  activateService = testingModule.get<ActivateService>(ActivateService);
+  apiKeyService = testingModule.get<ApiKeyService>(ApiKeyService);
+  communityService = testingModule.get<CommunityService>(CommunityService);
+  ethSignatureService =
+    testingModule.get<EthSignatureService>(EthSignatureService);
+  eventLogService = testingModule.get<EventLogService>(EventLogService);
+  periodsService = testingModule.get<PeriodsService>(PeriodsService);
+  periodsAssignmentService = testingModule.get<PeriodAssignmentsService>(
+    PeriodAssignmentsService,
+  );
+  periodSettingsService = testingModule.get<PeriodSettingsService>(
+    PeriodSettingsService,
+  );
+  praiseService = testingModule.get<PraiseService>(PraiseService);
+  quantificationsService = testingModule.get<QuantificationsService>(
+    QuantificationsService,
+  );
+  settingsService = testingModule.get<SettingsService>(SettingsService);
+  usersService = testingModule.get<UsersService>(UsersService);
+  userAccountsService =
+    testingModule.get<UserAccountsService>(UserAccountsService);
+
+  //Seeders
+  apiKeySeeder = testingModule.get<ApiKeySeeder>(ApiKeySeeder);
+  communitiesSeeder = testingModule.get<CommunitiesSeeder>(CommunitiesSeeder);
+  eventLogSeeder = testingModule.get<EventLogSeeder>(EventLogSeeder);
+  periodsSeeder = testingModule.get<PeriodsSeeder>(PeriodsSeeder);
+  periodSettingsSeeder =
+    testingModule.get<PeriodSettingsSeeder>(PeriodSettingsSeeder);
+  praiseSeeder = testingModule.get<PraiseSeeder>(PraiseSeeder);
+  quantificationsSeeder = testingModule.get<QuantificationsSeeder>(
+    QuantificationsSeeder,
+  );
+  settingsSeeder = testingModule.get<SettingsSeeder>(SettingsSeeder);
+  userAccountsSeeder =
+    testingModule.get<UserAccountsSeeder>(UserAccountsSeeder);
+  usersSeeder = testingModule.get<UsersSeeder>(UsersSeeder);
 }
 
 beforeAll(async () => {
   await startNest();
+});
+
+afterAll(() => {
+  app.close();
 });
--- packages/api2/test/shared/request.ts
+++ packages/api2/test/shared/request.ts
@@ -3,9 +3,6 @@ import { INestApplication } from '@nestjs/common';
 import { Wallet } from 'ethers';
 import { TestingModule } from '@nestjs/testing';
 import { EthSignatureService } from '@/auth/eth-signature.service';
-import { ClassConstructor, plainToInstance } from 'class-transformer';
-import { validate } from 'class-validator';
-import '@testing-library/jest-dom';
 
 export const loginUser = async (
   app: INestApplication,
@@ -91,54 +88,3 @@ export const authorizedDeleteRequest = (
     .delete(url)
     .set('Authorization', `Bearer ${accessToken}`);
 };
-
-expect.extend({
-  /**
-   * Custom jest matcher to check if an object has been properly serialized by NestJS.
-   */
-  toBeProperlySerialized(received: any) {
-    // If the object has a __v property, it has not been properly serialized.
-    const fail = received.hasOwnProperty('__v');
-    if (fail) {
-      return {
-        message: () => `expected object to be serialized but was not`,
-        pass: false,
-      };
-    } else {
-      return {
-        message: () => `expected object to be serialized`,
-        pass: true,
-      };
-    }
-  },
-});
-
-expect.extend({
-  /**
-   * Custom jest matcher to check if an object is valid according to a class.
-   */
-  async toBeValidClass(received: any, expectedClass: ClassConstructor<any>) {
-    const instance = plainToInstance(expectedClass, received);
-    const validationErrors = await validate(instance);
-    const pass = validationErrors.length === 0;
-    if (pass) {
-      return {
-        message: () =>
-          `expected object to be valid according to class ${expectedClass.name}`,
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `expected object to be valid according to class ${
-            expectedClass.name
-          } but had the following validation errors:\n\nValidation errors: ${JSON.stringify(
-            validationErrors,
-            undefined,
-            2,
-          )}`,
-        pass: false,
-      };
-    }
-  },
-});
