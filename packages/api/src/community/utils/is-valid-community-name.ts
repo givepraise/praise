460bd4bd4c4878c04881e8321031346bb5c1d9ae
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-04-12 10:57:36 +0330

    Add some names to blacklist for communities
    related #924

--- packages/api/src/community/utils/is-valid-community-name.ts
+++ packages/api/src/community/utils/is-valid-community-name.ts
@@ -1,3 +1,25 @@
+const communityNameBlacklist = [
+  'www',
+  'setup',
+  'admin',
+  'api',
+  'app',
+  'mail',
+  'docs',
+  'blog',
+  'help',
+  'support',
+  'status',
+  'about',
+  'contact',
+  'terms',
+  'privacy',
+  'tos',
+  'legal',
+  'security',
+]
+
+
 /**
  * A valid community name is:
  * - lowercase
@@ -8,7 +30,18 @@
  * - cannot end with a dot or hyphen
  * - cannot contain two dots, two hyphens, or two underscores in a row
  */
-export function isValidCommunityName(name: string) {
+
+export function checkCommunityNameRegex(name: string): boolean {
   const pattern = /^[a-z0-9][a-z0-9_.-]{1,18}[a-z0-9]$/;
   return pattern.test(name);
+}
+
+
+export function isValidCommunityName(name: string) :boolean{
+  const isRegexValid = checkCommunityNameRegex(name);
+  if (!isRegexValid){
+    return false
+  }
+  return !communityNameBlacklist.includes(name);
+
 }
