9cfbd1f9de7d7a0bcdb6d60e3a55ebb536f26448
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-04-12 11:14:06 +0330

    Change in frontend and api to make github actions run

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -5,7 +5,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { version } from '../package.json';
-
 import { AppModule } from './app.module';
 import { AppConfig } from './shared/appConfig.shared';
 import { envCheck } from './shared/env.shared';
