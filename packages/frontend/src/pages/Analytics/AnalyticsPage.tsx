7790b7cd4d368ab4507afc20bf68cdf2bc40ac8e
kristoferlund <kristofer@fmckl.se>
2023-04-12 08:55:38 +0200

    WIP

--- packages/frontend/src/pages/Analytics/AnalyticsPage.tsx
+++ packages/frontend/src/pages/Analytics/AnalyticsPage.tsx
@@ -1,69 +1,53 @@
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { BreadCrumb } from '../../components/ui/BreadCrumb';
 import { Page } from '../../components/ui/Page';
-import { PeriodDetailsDto } from '../../model/periods/dto/period-details.dto';
-import {
-  AllPeriods,
-  useLoadSinglePeriodDetails,
-  SinglePeriod,
-} from '../../model/periods/periods';
-import { getPreviousPeriod } from '../../utils/periods';
 import { DuckDb } from './components/DuckDb';
 import PraiseOverTime from './components/PraiseOverTime';
-import { differenceInDays, parseISO, subDays } from 'date-fns';
+import { differenceInDays, subDays } from 'date-fns';
 import TopPraiseGivers from './components/TopPraiseGivers';
 import TopPraiseReceivers from './components/TopPraiseReceivers';
 import PraiseScore from './components/PraiseScore';
+import {
+  DatePeriodRange,
+  DatePeriodRangeEndDate,
+  DatePeriodRangeStartDate,
+} from './components/DatePeriodRange';
 
-const AnalyticsPage = (): JSX.Element | null => {
-  const periodId = '6258683641e6d2310a9f4449';
-  const allPeriods = useRecoilValue(AllPeriods);
-  useLoadSinglePeriodDetails(periodId); // Fetch additional period details
-  const period = useRecoilValue(SinglePeriod(periodId));
-  const [previousPeriod, setPreviousPeriod] =
-    React.useState<PeriodDetailsDto>();
+const Graphs = (): JSX.Element | null => {
+  const date2 = useRecoilValue(DatePeriodRangeStartDate);
+  const date3 = useRecoilValue(DatePeriodRangeEndDate);
 
-  // Start date = end date of previous period
-  React.useEffect(() => {
-    if (period) {
-      const previousPeriod = getPreviousPeriod(allPeriods, period);
-      if (previousPeriod) {
-        setPreviousPeriod(previousPeriod);
-      }
-    }
-  }, [period, allPeriods]);
-
-  if (!period || !previousPeriod) return null;
-
-  const date2Str = previousPeriod.endDate;
-  const date2 = parseISO(previousPeriod.endDate);
-
-  const date3Str = period.endDate;
-  const date3 = parseISO(period.endDate);
+  if (!date2 || !date3) return null;
 
   const daysDiff = differenceInDays(date3, date2);
-
   const date1 = subDays(date2, daysDiff);
   const date1Str = date1.toISOString();
+  const date2Str = date2.toISOString();
+  const date3Str = date3.toISOString();
 
+  return (
+    <DuckDb startDate={date1Str} endDate={date3Str}>
+      <div className="h-full gap-5 columns-1 md:columns-3">
+        <PraiseOverTime date1={date1Str} date2={date2Str} date3={date3Str} />
+        <PraiseScore date1={date1Str} date2={date2Str} date3={date3Str} />
+        <TopPraiseGivers date1={date1Str} date2={date2Str} date3={date3Str} />
+        <TopPraiseReceivers
+          date1={date1Str}
+          date2={date2Str}
+          date3={date3Str}
+        />
+      </div>
+    </DuckDb>
+  );
+};
+const AnalyticsPage = (): JSX.Element | null => {
   return (
     <Page variant="full">
       <BreadCrumb name="Analytics" icon={faCalendarAlt} />
 
-      <DuckDb startDate={date1Str} endDate={date3Str}>
-        <div className="h-full gap-5 columns-1 md:columns-3">
-          <PraiseOverTime date1={date1Str} date2={date2Str} date3={date3Str} />
-          <PraiseScore date1={date1Str} date2={date2Str} date3={date3Str} />
-          <TopPraiseGivers date1={date1Str} date2={date2Str} date3={date3Str} />
-          <TopPraiseReceivers
-            date1={date1Str}
-            date2={date2Str}
-            date3={date3Str}
-          />
-        </div>
-      </DuckDb>
+      <DatePeriodRange />
+      <Graphs />
     </Page>
   );
 };
---
+++ packages/frontend/src/pages/Analytics/components/DatePeriodRange.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react';
+import { useRecoilState, atom, useRecoilValue } from 'recoil';
+import { AllPeriods } from '../../../model/periods/periods';
+import { getPeriodDatesConfig } from '../../../model/report/util/get-period-dates-config';
+import { toast } from 'react-hot-toast';
+
+export const DatePeriodRangePeriod = atom<string>({
+  key: 'DatePeriodRangePeriod',
+  default: '',
+});
+
+export const DatePeriodRangeStartDate = atom<Date | null>({
+  key: 'DatePeriodRangeStartDate',
+  default: null,
+});
+
+export const DatePeriodRangeEndDate = atom<Date | null>({
+  key: 'DatePeriodRangeEndDate',
+  default: null,
+});
+
+export const DatePeriodRange: React.FC = () => {
+  const allPeriods = useRecoilValue(AllPeriods);
+  const [periodId, setPeriodId] = useRecoilState(DatePeriodRangePeriod);
+  const [startDate, setStartDate] = useRecoilState(DatePeriodRangeStartDate);
+  const [endDate, setEndDate] = useRecoilState(DatePeriodRangeEndDate);
+
+  useEffect(() => {
+    if (periodId) {
+      try {
+        const dates = getPeriodDatesConfig(allPeriods, periodId);
+        if (!dates) return;
+        setStartDate(new Date(dates.startDate));
+        setEndDate(new Date(dates.endDate));
+      } catch (err) {
+        toast.error((err as Error).message);
+      }
+    }
+  }, [periodId, setStartDate, setEndDate, allPeriods]);
+
+  const handleStartDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setPeriodId('');
+    setStartDate(new Date(event.target.value));
+  };
+
+  const handleEndDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setPeriodId('');
+    setEndDate(new Date(event.target.value));
+  };
+
+  return (
+    <div className="w-full p-5 mb-5 text-sm border rounded-none shadow-none md:shadow-md md:rounded-xl bg-warm-gray-50 dark:bg-slate-600 break-inside-avoid-column">
+      <div className="flex justify-start space-x-5">
+        <div className="flex items-center">Select a period:</div>
+        <select
+          value={periodId}
+          onChange={(e): void => setPeriodId(e.target.value)}
+          className="text-sm"
+        >
+          <option value="" disabled>
+            Period
+          </option>
+          {allPeriods.map((period) => {
+            return (
+              <option key={period._id} value={period._id}>
+                {period.name}
+              </option>
+            );
+          })}
+        </select>
+        <div className="flex items-center">or dates:</div>
+        <input
+          type="date"
+          value={startDate ? startDate.toISOString().substr(0, 10) : ''}
+          onChange={handleStartDateChange}
+        />
+        <div className="flex items-center">to</div>
+        <input
+          type="date"
+          value={endDate ? endDate.toISOString().substr(0, 10) : ''}
+          onChange={handleEndDateChange}
+        />
+      </div>
+    </div>
+  );
+};
