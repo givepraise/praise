caf264f96f54eafac33d6e6e59e4e6b27f737e29
kristoferlund <kristofer@fmckl.se>
2023-04-14 11:25:51 +0200

    Openapi and schema rebuild

--- packages/api-types/out/schema.ts
+++ packages/api-types/out/schema.ts
@@ -173,6 +173,7 @@ export interface paths {
     post: operations["CommunityController_create"];
   };
   "/api/communities/domain-check": {
+    /** Check to see if a given community hostname/domain name is setup and served by the API. Endpoint does not require authentication. */
     get: operations["CommunityController_domainCheck"];
   };
   "/api/communities/{id}": {
@@ -1799,6 +1800,7 @@ export interface operations {
       };
     };
   };
+  /** Check to see if a given community hostname/domain name is setup and served by the API. Endpoint does not require authentication. */
   CommunityController_domainCheck: {
     parameters: {
       query: {
