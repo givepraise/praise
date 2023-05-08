c5b13e01aa4756f4f73586e20d54b560d297a2ed
kristoferlund <kristofer@fmckl.se>
2023-05-08 14:52:39 +0200

    Connect using docker compose service name if running in Docker

---
+++ packages/discord-bot/src/utils/isDocker.ts
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+
+let isDockerCached: boolean;
+
+function hasDockerEnv(): boolean {
+  try {
+    fs.statSync('/.dockerenv');
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function hasDockerCGroup(): boolean {
+  try {
+    return fs.readFileSync('/proc/self/cgroup', 'utf8').includes('docker');
+  } catch {
+    return false;
+  }
+}
+
+export function isDocker(): boolean {
+  if (isDockerCached === undefined) {
+    isDockerCached = hasDockerEnv() || hasDockerCGroup();
+  }
+
+  return isDockerCached;
+}
