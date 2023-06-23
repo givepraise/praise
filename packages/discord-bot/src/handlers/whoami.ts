ec5a11d97ca0f7ce8ac0e0b00d48777be0d3781b
Vy <vyom.j@protonmail.com>
2023-06-24 00:33:41 +0530

    fix: drop last active date

--- packages/discord-bot/src/handlers/whoami.ts
+++ packages/discord-bot/src/handlers/whoami.ts
@@ -58,7 +58,6 @@ export const whoamiHandler: CommandHandler = async (
           platform: account.platform,
           user: account.name,
           activationDate: account.createdAt,
-          latestUsageDate: account.updatedAt,
         });
       }
     } else {
@@ -67,7 +66,6 @@ export const whoamiHandler: CommandHandler = async (
           platform: 'DISCORD',
           user: ua.name,
           activationDate: ua.createdAt,
-          latestUsageDate: ua.updatedAt,
         });
       }
     }
