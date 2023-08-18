22fb5e5337f259ac4213dd96aa0648d1e637a901
Kristofer <kristofer@fmckl.se>
2023-08-18 10:37:58 +0200

    Add explanatory texts

--- packages/frontend/src/pages/Settings/components/ApplicationSettings.tsx
+++ packages/frontend/src/pages/Settings/components/ApplicationSettings.tsx
@@ -6,6 +6,7 @@ import ApplicationSettingsApiKeys from './ApplicationSettingsApiKeys';
 import { Setting as SettingDto } from '@/model/settings/dto/setting.dto';
 import { useRecoilValue } from 'recoil';
 import { CurrentCommunity } from '../../../model/community/community';
+import { Jazzicon } from '@ukstv/jazzicon-react';
 
 interface Params {
   settings: SettingDto[] | undefined;
@@ -28,13 +29,39 @@ export const ApplicationSettings = ({
             <div className="flex flex-col gap-4 mb-4">
               <div>
                 <label className="block font-bold group">Creator address</label>
-                <div>{community?.creator}</div>
+                <div className="mb-2 text-sm text-warm-gray-400">
+                  The creator address is the address that represents this
+                  community onchain. To be able to create attestations or
+                  distribute tokens from this address, it should be a{' '}
+                  <a
+                    href="https://safe.global"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Safe
+                  </a>{' '}
+                  address.
+                </div>
+                <div>
+                  <Jazzicon
+                    address={community?.creator || ''}
+                    className="inline-block w-4"
+                  />{' '}
+                  {community?.creator}
+                </div>
               </div>
               <div>
                 <label className="block font-bold group">Owner addresses</label>
+                <div className="mb-2 text-sm text-warm-gray-400">
+                  The owner addresses represent the identities that will allways
+                  have adin permissions in this Praise community.
+                </div>
                 <div>
                   {community?.owners.map((owner) => (
-                    <div key={owner}>{owner}</div>
+                    <div key={owner}>
+                      <Jazzicon address={owner} className="inline-block w-4" />{' '}
+                      {owner}
+                    </div>
                   ))}
                 </div>
               </div>
