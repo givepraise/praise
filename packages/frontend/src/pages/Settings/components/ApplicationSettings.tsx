ba4bd16143543c361756456fc1163ea43cb58161
Kristofer <kristofer@fmckl.se>
2023-08-18 10:20:00 +0200

    Display community details on settings page

--- packages/frontend/src/pages/Settings/components/ApplicationSettings.tsx
+++ packages/frontend/src/pages/Settings/components/ApplicationSettings.tsx
@@ -4,6 +4,8 @@ import { Box } from '@/components/ui/Box';
 import { SettingsSubgroup } from './SettingsSubgroup';
 import ApplicationSettingsApiKeys from './ApplicationSettingsApiKeys';
 import { Setting as SettingDto } from '@/model/settings/dto/setting.dto';
+import { useRecoilValue } from 'recoil';
+import { CurrentCommunity } from '../../../model/community/community';
 
 interface Params {
   settings: SettingDto[] | undefined;
@@ -16,13 +18,29 @@ export const ApplicationSettings = ({
   settings,
   parentOnSubmit,
 }: Params): JSX.Element | null => {
+  const community = useRecoilValue(CurrentCommunity);
   if (!settings) return null;
-
   return (
     <>
       <Box className="mb-6">
         <SettingsSubgroup header="Application Settings">
-          <SettingsForm settings={settings} parentOnSubmit={parentOnSubmit} />
+          <>
+            <div className="flex flex-col gap-4 mb-4">
+              <div>
+                <label className="block font-bold group">Creator address</label>
+                <div>{community?.creator}</div>
+              </div>
+              <div>
+                <label className="block font-bold group">Owner addresses</label>
+                <div>
+                  {community?.owners.map((owner) => (
+                    <div key={owner}>{owner}</div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <SettingsForm settings={settings} parentOnSubmit={parentOnSubmit} />
+          </>
         </SettingsSubgroup>
       </Box>
       <Box className="mb-6">
