67b2e81c9b1eef0fd3741fd7ce11b3442243ef2c
Kristofer <kristofer@fmckl.se>
2023-09-22 17:47:52 +0200

    Mark period.attestationsTxHash as optional

--- packages/api/src/periods/schemas/periods.schema.ts
+++ packages/api/src/periods/schemas/periods.schema.ts
@@ -3,7 +3,7 @@ import { Types } from 'mongoose';
 import { PeriodStatusType } from '../enums/status-type.enum';
 import { ExposeId } from '../../shared/decorators/expose-id.decorator';
 import { ApiProperty, ApiResponseProperty } from '@nestjs/swagger';
-import { IsString, Length } from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 export type PeriodDocument = Period & Document;
@@ -48,8 +48,10 @@ export class Period {
     example:
       '0x46164b8581258eec4b4f44d626925953d0d7581514d9fd1335e3bd660d48e07c',
     type: 'string',
+    required: false,
   })
   @IsString()
+  @IsOptional()
   @Prop({ required: false })
   attestationsTxHash?: string;
 
