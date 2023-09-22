0279ade9bee63bfbf7936622e08c7253bb0b44f8
Kristofer <kristofer@fmckl.se>
2023-09-22 15:43:15 +0200

    Change to production attestation constants

--- packages/frontend/src/model/eas/eas.constants.ts
+++ packages/frontend/src/model/eas/eas.constants.ts
@@ -1,10 +1,10 @@
 export const EAS_ADDRESS = '0x4200000000000000000000000000000000000021';
 
 export const ATTESTATION_SCHEMA_UID =
-  '0x569cab01c87d76b5e85706a4eca2a10a7499758a3e7aa0e81feeb3aaf95255be';
+  '0x1c622d3fdb24759ed71266b218c4750ad54549a7612451851f3b0432ce167e68';
 
 export const ATTESTATION_SCHEMA =
-  'string period,uint16 received_score,uint16 given_score,bool top_10_receiver,bool top_50_receiver,bool top_100_receiver,bool top_10_giver,bool top_50_giver,bool top_100_giver';
+  'string username,string period,uint16 received_score,uint16 given_score,bool top_10_receiver,bool top_50_receiver,bool top_100_receiver,bool top_10_giver,bool top_50_giver,bool top_100_giver';
 
 export const ATTESTATION_REPORT_MANIFEST_URL =
   'https://raw.githubusercontent.com/givepraise/reports/main/reports/attestations/manifest.json';
