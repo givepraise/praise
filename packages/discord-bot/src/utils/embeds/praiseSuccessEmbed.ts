a4912660fa887ad93d3635fd6c4af559ad142431
Kristofer <kristofer@fmckl.se>
2023-12-15 11:50:01 +0100

    Hide receipient in Discord feed when DISCORD_BOT_PRAISE_NOTIFICATIONS_ENABLED is disabled

--- packages/discord-bot/src/utils/embeds/praiseSuccessEmbed.ts
+++ packages/discord-bot/src/utils/embeds/praiseSuccessEmbed.ts
@@ -5,11 +5,17 @@ export const praiseSuccessEmbed = async (
   user: User,
   receivers: string[],
   reason: string,
-  host: string
+  host: string,
+  praiseNotificationsEnabled = true
 ): Promise<EmbedBuilder> => {
-  const msg = ((await getSetting('PRAISE_SUCCESS_MESSAGE', host)) as string)
-    .replace('{@receivers}', `${receivers.join(', ')}`)
-    .replace('{reason}', reason);
+  let msg = (await getSetting('PRAISE_SUCCESS_MESSAGE', host)) as string;
+
+  if (!praiseNotificationsEnabled) {
+    msg = msg.replace('{@receivers}', '[HIDDEN]');
+  } else {
+    msg = msg.replace('{@receivers}', `${receivers.join(', ')}`);
+  }
+  msg = msg.replace('{reason}', reason);
 
   const embed = new EmbedBuilder().setColor(0xe6007e).setDescription(msg);
   return embed;
